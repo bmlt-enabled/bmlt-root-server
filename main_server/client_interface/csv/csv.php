9583d1534eff4100e43ed7d680d900aa046cde83
chris@littlegreenviper.com <webservant@magnaws.org>
2013-01-16 21:35:31 -0500

    Fixed a stupid minor bug with the line endings in the CSV file.

--- main_server/client_interface/csv/csv.php
+++ main_server/client_interface/csv/csv.php
@@ -140,7 +140,7 @@ function parse_redirect (
 			if ( isset ( $http_vars['xml_data'] ) )
 				{
                 $result = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-				$xsd_uri = 'http://'.htmlspecialchars ( str_replace ( '/client_interface/xml', '/client_interface/xsd', $_SERVER['SERVER_NAME'].dirname ( $_SERVER['SCRIPT_NAME'] ).'/GetFormats.php' ) );
+				$xsd_uri = 'http://'.htmlspecialchars ( str_replace ( '/client_interface/xml', '/client_interface/xsd', $_SERVER['SERVER_NAME'].dirname ( $_SERVER['SCRIPT_NAME'] ).'/GetServiceBodies.php' ) );
 				$result .= "<formats xmlns=\"http://".$_SERVER['SERVER_NAME']."\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://".$_SERVER['SERVER_NAME']." $xsd_uri\">";
 				$result .= TranslateToXML ( $result2 );
 				$result .= "</formats>";
@@ -449,7 +449,7 @@ function GetServiceBodies   (
 		{
 		}
 
-	return implode ( "\r", $ret );
+	return implode ( "\n", $ret );
     }
 
 /*******************************************************************/
@@ -707,7 +707,7 @@ function HandleDefault (
 						$in_http_vars	///< The HTTP GET and POST parameters.
 						)
 	{
-	return "You must supply one of the following: 'switcher=GetSearchResults', 'switcher=GetFormats' or 'switcher=GetChanges'";
+	return "You must supply one of the following: 'switcher=GetSearchResults', 'switcher=GetFormats', 'switcher=GetChanges' or 'switcher=GetServiceBodies'.";
 	}
 
 /*******************************************************************/
