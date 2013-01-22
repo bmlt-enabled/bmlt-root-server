8f6135ec04c02db442df663b26d7f0e5a8bbf27d
chris@littlegreenviper.com <webservant@magnaws.org>
2013-01-22 10:10:10 -0500

    Updated the URI in the headers.

--- main_server/server/config/lang/de/format_codes.inc.php
+++ main_server/server/config/lang/de/format_codes.inc.php
@@ -8,7 +8,7 @@
     
     This file is part of the Basic Meeting List Toolbox (BMLT).
     
-    Find out more at: http://magshare.org/bmlt
+    Find out more at: http://bmlt.magshare.org
 
     BMLT is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
