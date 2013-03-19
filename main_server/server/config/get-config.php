f0edfd78bdeb50e8c7527026f9c556232be4b30e
chris@littlegreenviper.com <webservant@magnaws.org>
2013-03-19 08:22:30 -0400

    I now prevent the auto-config file from being in the old location (inside the directory).

--- main_server/server/config/get-config.php
+++ main_server/server/config/get-config.php
@@ -19,10 +19,7 @@
         }
     elseif ( file_exists ( dirname ( __FILE__ ).'/auto-config.inc.php' ) )
         {
-		if ( (fileperms ( dirname ( __FILE__ ).'/auto-config.inc.php' ) & 0x0002) == 0x0002 )
-		    {
-			die ('The auto-config.inc.php file is still writeable! Security requires that it be set to read-only!');
-		    }
-		
-        require ( dirname ( __FILE__ ).'/auto-config.inc.php' );
+        $realpath = realpath ( dirname ( __FILE__ ).'/../../../' );
+        $basename = basename ( realpath ( dirname ( __FILE__ ).'/../../' ) );
+		die ( sprintf ( 'The auto-config.inc.php file is in the wrong location! You need to move it out of this directory, and into the %s directory (the same directory as the %s directory)!', $realpath, $basename ) );
         }
