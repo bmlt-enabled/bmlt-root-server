d6ffda653afda4c4cedf81f31c9595b650398ea1
chris@littlegreenviper.com <webservant@magnaws.org>
2013-05-01 21:18:47 -0400

    Hopefully, added a workaround for servers with dicey session support.

--- main_server/index.php
+++ main_server/index.php
@@ -18,6 +18,7 @@
     along with this code.  If not, see <http://www.gnu.org/licenses/>.
 */
 // define ( '__DEBUG_MODE__', 1 ); // Uncomment to make the CSS and JavaScript easier to trace (and less efficient).
+session_start();
 define ( 'BMLT_EXEC', 1 );
 include ( dirname ( __FILE__ ).'/local_server/index.php' );
 ?>
