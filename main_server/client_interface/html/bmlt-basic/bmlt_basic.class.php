c282ea3188cf564123b68f070c0d8d8439e066cf
chris@littlegreenviper.com <webservant@magnaws.org>
2014-02-24 15:26:32 -0500

    Fixed issues with odd server ports and Windows servers.

--- main_server/client_interface/html/bmlt-basic/bmlt_basic.class.php
+++ main_server/client_interface/html/bmlt-basic/bmlt_basic.class.php
@@ -80,7 +80,7 @@ class bmlt_basic extends BMLTPlugin
     ****************************************************************************************/
     protected function get_ajax_base_uri()
         {
-        $ret = 'http://'.$_SERVER['SERVER_NAME'].(($_SERVER['SERVER_PORT'] != 80) ? ':'.$_SERVER['SERVER_PORT'] : '').$_SERVER['SCRIPT_NAME'];
+        $ret = 'http://'.$_SERVER['SERVER_NAME'].(($_SERVER['SERVER_PORT'] != 80) ? ':'.$_SERVER['SERVER_PORT'] : '').str_replace ( '\\', '/', $_SERVER['PHP_SELF'] );
         return $ret;
         }
     
