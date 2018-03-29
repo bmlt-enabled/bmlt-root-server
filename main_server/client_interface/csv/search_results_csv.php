8689d97939ca43831bfa4451f45292cf68ce34ad
Chris M <webmaster@magnaws.com>
2018-03-29 17:39:48 -0400

    Tweaked the old pagination stuff to use the new RFC.

--- main_server/client_interface/csv/search_results_csv.php
+++ main_server/client_interface/csv/search_results_csv.php
@@ -267,6 +267,12 @@ function DisplaySearchResultsCSV ( $in_http_vars,	/**< The various HTTP GET and 
                                                                 
                                                             - 'simple_other_fields'
                                                                 Set this to '1' in order to prevent the server from separating values with the prompt separator.
+                                                                
+                                                            - 'page_size'
+                                                                This is an integer that determines the maximum number of meetings to include in one return. If it is not specified, or set to 0 (intval), then all meetings are returned.
+                                                                
+                                                            - 'page_num'
+                                                                This is an integer (1-based) that is ignored if 'page_size' is not defined or set to 0. This is the page number offset to return. 1 or 0 is the first page. every page after 1 is the following page of results.
 											*/
 									&$return_array = null,	///< If this is supplied, then the result will be saved in this as an array. It must be an empty array, supplied by reference.
 								    &$return_geocode = null,    /**< If this is supplied, the response will be an associative array, with the search center and radius.
@@ -316,9 +322,9 @@ function DisplaySearchResultsCSV ( $in_http_vars,	/**< The various HTTP GET and 
 	
 		SetUpSearch ( $search_manager, $in_http_vars );
 		
-		if ( isset ( $in_http_vars['page_display_size'] ) )
+		if ( isset ( $in_http_vars['page_size'] ) )
 			{
-			$search_manager->SetResultsPerPage ( $in_http_vars['page_display_size'] );
+			$search_manager->SetResultsPerPage ( $in_http_vars['page_size'] );
 			}
 		
 		if ( isset ( $in_http_vars['sort_dir'] ) )
@@ -377,9 +383,9 @@ function DisplaySearchResultsCSV ( $in_http_vars,	/**< The various HTTP GET and 
 			
 		$page_no = 1;
 		
-		if ( isset ( $in_http_vars['page'] ) && (0 < intval ( $in_http_vars['page'] )) )
+		if ( isset ( $in_http_vars['page_num'] ) && (0 < intval ( $in_http_vars['page_num'] )) )
 			{
-			$page_no = intval ( $in_http_vars['page'] );
+			$page_no = intval ( $in_http_vars['page_num'] );
 			}
 		
 		if ( 1 > intval ( $page_no ) )
