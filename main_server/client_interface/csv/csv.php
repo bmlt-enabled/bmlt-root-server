81c427d8c0b73bfa90378c5c8a11fb644c1ea73b
pjaudiomv <pjaudiomv@gmail.com>
2018-10-06 11:20:53 -0400

    add meeting_states_and_provinces to GetServerInfo

--- main_server/client_interface/csv/csv.php
+++ main_server/client_interface/csv/csv.php
@@ -1299,7 +1299,7 @@ function GetServerInfo()
             }
         }
     
-    $ret = '"version","versionInt","langs","nativeLang","centerLongitude","centerLatitude","centerZoom","defaultDuration","regionBias","charSet","distanceUnits","semanticAdmin","emailEnabled","emailIncludesServiceBodies","changesPerMeeting","available_keys","google_api_key"'."\n";
+    $ret = '"version","versionInt","langs","nativeLang","centerLongitude","centerLatitude","centerZoom","defaultDuration","regionBias","charSet","distanceUnits","semanticAdmin","emailEnabled","emailIncludesServiceBodies","changesPerMeeting","meeting_states_and_provinces","available_keys","google_api_key"'."\n";
     $ret .= '"'.$version_string.'","'.strval ( $version_num ).'","'.$lang_string.'","'.$default_lang.'",';
     $ret .= '"'.strval( $localStrings['search_spec_map_center']['longitude'] ).'","'.strval( $localStrings['search_spec_map_center']['latitude'] ).'",';
     $ret .= '"'.strval( $localStrings['search_spec_map_center']['zoom'] ).'","'.$localStrings['default_duration_time'].'",';
