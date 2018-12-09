b1714ac26f8c0669e7947a11fc32b0aedcb0b2db
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2018-12-08 19:38:39 -0800

    Respect new settings in docker image

--- docker/start-bmlt.sh
+++ docker/start-bmlt.sh
@@ -4,6 +4,8 @@ sed -i '/dbUser/d' /var/www/html/auto-config.inc.php
 sed -i '/dbPassword/d' /var/www/html/auto-config.inc.php
 sed -i '/dbServer/d' /var/www/html/auto-config.inc.php
 sed -i '/dbPrefix/d' /var/www/html/auto-config.inc.php
+sed -i '/force_https_urls/d' /var/www/html/auto-config.inc.php
+sed -i '/do_not_force_port/d' /var/www/html/auto-config.inc.php
 
 if [ ! -z $GKEY ]
 then
@@ -33,6 +35,16 @@ fi
 if [ ! -z $DBPREFIX ]
 then
     echo "\$dbPrefix = \"$DBPREFIX\";" >> /var/www/html/auto-config.inc.php
+fi
+
+if [ ! -z $FORCE_HTTPS_URLS -a "$FORCE_HTTPS_URLS" == "true" ]
+then
+    echo "\$g_force_https_urls = true;" >> /var/www/html/auto-config.inc.php
+fi
+
+if [ ! -z $DO_NOT_FORCE_PORT -a "$DO_NOT_FORCE_PORT" == "true" ]
+then
+    echo "\$g_do_not_force_port = true;" >> /var/www/html/auto-config.inc.php
 fi
 
 apachectl -D FOREGROUND
