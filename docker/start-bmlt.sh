528f8b1639c41ce06ad83ae79795aa021df6698a
jbraswell <10187286+jbraswell@users.noreply.github.com>
2018-12-10 11:27:32 -0500

    Use HTTP_X_FORWARDED_PROTO (#110)

--- docker/start-bmlt.sh
+++ docker/start-bmlt.sh
@@ -4,7 +4,6 @@ sed -i '/dbUser/d' /var/www/html/auto-config.inc.php
 sed -i '/dbPassword/d' /var/www/html/auto-config.inc.php
 sed -i '/dbServer/d' /var/www/html/auto-config.inc.php
 sed -i '/dbPrefix/d' /var/www/html/auto-config.inc.php
-sed -i '/force_https_urls/d' /var/www/html/auto-config.inc.php
 sed -i '/do_not_force_port/d' /var/www/html/auto-config.inc.php
 
 if [ ! -z $GKEY ]
@@ -35,11 +34,6 @@ fi
 if [ ! -z $DBPREFIX ]
 then
     echo "\$dbPrefix = \"$DBPREFIX\";" >> /var/www/html/auto-config.inc.php
-fi
-
-if [ ! -z $FORCE_HTTPS_URLS -a "$FORCE_HTTPS_URLS" == "true" ]
-then
-    echo "\$g_force_https_urls = true;" >> /var/www/html/auto-config.inc.php
 fi
 
 if [ ! -z $DO_NOT_FORCE_PORT -a "$DO_NOT_FORCE_PORT" == "true" ]
