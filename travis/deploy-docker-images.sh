e2a55934b8ca235db7b30293d80581ce8b760e36
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2018-12-11 07:51:10 -0800

    Tag and pushdocker images with git commit hash

--- travis/deploy-docker-images.sh
+++ travis/deploy-docker-images.sh
@@ -1,24 +1,23 @@
-tag() {
+tag_and_push() {
     SOURCE_IMAGE=$1
-    TARGET_IMAGE=$2
+    TARGET_IMAGE_NAME=$2
 
     if [ ! -z $TRAVIS_TAG ]
     then
-        TARGET_IMAGE="$TARGET_IMAGE:$TRAVIS_TAG"
+        TARGET_IMAGE1="$TARGET_IMAGE_NAME:$TRAVIS_TAG"
     else
-        TARGET_IMAGE="$TARGET_IMAGE:latest"
+        TARGET_IMAGE1="$TARGET_IMAGE_NAME:latest"
     fi
 
-    docker tag $SOURCE_IMAGE $TARGET_IMAGE
+    TARGET_IMAGE2="$TARGET_IMAGE_NAME:$TRAVIS_COMMIT"
 
-    echo $TARGET_IMAGE
+    docker tag $SOURCE_IMAGE $TARGET_IMAGE1
+    docker tag $SOURCE_IMAGE $TARGET_IMAGE2
+
+    docker push $TARGET_IMAGE1
+    docker push $TARGET_IMAGE2
 }
 
-
-push() {
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker push $1
-}
-
-push $(tag "bmlt-root-server:$TRAVIS_COMMIT" bmltenabled/bmlt-root-server)
-push $(tag "bmlt-root-server-sample-db:$TRAVIS_COMMIT" bmltenabled/bmlt-root-server-sample-db)
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+tag_and_push "bmlt-root-server:$TRAVIS_COMMIT" bmltenabled/bmlt-root-server
+tag_and_push "bmlt-root-server-sample-db:$TRAVIS_COMMIT" bmltenabled/bmlt-root-server-sample-db
