dc7926c92c88aa1a2765c5bf8fe9a3bff0e22774
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2018-12-23 19:41:17 -0800

    Reformat danish language files PSR-2

--- main_server/local_server/server_admin/lang/dk/install_wizard_strings.php
+++ main_server/local_server/server_admin/lang/dk/install_wizard_strings.php
@@ -1,7 +1,7 @@
 <?php
 /***********************************************************************/
 /** This file is part of the Basic Meeting List Toolbox (BMLT).
-    
+
     Find out more at: http://bmlt.magshare.org
 
     BMLT is free software: you can redistribute it and/or modify
@@ -16,19 +16,19 @@
 
     You should have received a copy of the GNU General Public License
     along with this code.  If not, see <http://www.gnu.org/licenses/>.*/
-	defined( 'BMLT_EXEC' ) or die ( 'Cannot Execute Directly' );	// Makes sure that this file is in the correct context.
-	
-	global $comdef_install_wizard_strings;
-	
-	$comdef_install_wizard_strings = array (
-	                                        'Database_Version_Error'        =>  'FEJL: Du skal have PHP Version 5.6 eller nyere installeret på denne server!',
-	                                        'Database_PDO_Error'            =>  'FEJL: Du har ikke PHP PDO installeret!',
-	                                        'Database_Type_Error'           =>  'FEJL: Selvom du har PDO, har du ikke installeret databasedrivere!',
-	                                        'Database_Type_MySQL_Error'     =>  'FEJL: Selvom du har PDO, og du har installeret database drivere, er ingen af MySQL (den eneste understøttede driver)!',
-	                                        'Database_TestButton_Text'      =>  'TEST',
-	                                        'Database_TestButton_Success'   =>  'Databaseforbindelsen var vellykket.',
-	                                        'Database_TestButton_Fail'      =>  'Databaseforbindelsen mislykkedes: ',
-	                                        'Database_TestButton_Fail2'     =>  'Databaseforbindelsen mislykkedes, fordi der allerede er en initialiseret database.',
+    defined('BMLT_EXEC') or die('Cannot Execute Directly');    // Makes sure that this file is in the correct context.
+    
+    global $comdef_install_wizard_strings;
+    
+    $comdef_install_wizard_strings = array (
+                                            'Database_Version_Error'        =>  'FEJL: Du skal have PHP Version 5.6 eller nyere installeret på denne server!',
+                                            'Database_PDO_Error'            =>  'FEJL: Du har ikke PHP PDO installeret!',
+                                            'Database_Type_Error'           =>  'FEJL: Selvom du har PDO, har du ikke installeret databasedrivere!',
+                                            'Database_Type_MySQL_Error'     =>  'FEJL: Selvom du har PDO, og du har installeret database drivere, er ingen af MySQL (den eneste understøttede driver)!',
+                                            'Database_TestButton_Text'      =>  'TEST',
+                                            'Database_TestButton_Success'   =>  'Databaseforbindelsen var vellykket.',
+                                            'Database_TestButton_Fail'      =>  'Databaseforbindelsen mislykkedes: ',
+                                            'Database_TestButton_Fail2'     =>  'Databaseforbindelsen mislykkedes, fordi der allerede er en initialiseret database.',
 
                                             'AJAX_Handler_DB_Connect_Error' =>  'Databaseforbindelsen mislykkedes! Sørg for, at databasen eksisterer, ER HELT TOM, brugeren er oprettet, og den bruger har fuld tilladelse på den tomme database.',
                                             'AJAX_Handler_DB_Established_Error' =>  'Databasen eksisterer allerede, og er blevet oprettet! Du kan ikke bruge denne opsætning til at overskrive en eksisterende database!',
@@ -39,48 +39,48 @@
                                             'NoServerAdmin_Note_AlreadySet' =>  'Der findes allerede en eksisterende database, så du kan ikke oprette en serveradministratorkonto (der eksisterer allerede en).',
                                             'NeedLongerPasswordNote'        =>  'Denne adgangskode er for kort. Det skal være mindst %d tegn langt.',
                                             
-	                                        'Prev_Button'                   =>  'FORRIGE',
-	                                        'Next_Button'                   =>  'NÆSTE',
+                                            'Prev_Button'                   =>  'FORRIGE',
+                                            'Next_Button'                   =>  'NÆSTE',
 
-	                                        'Page_1_Tab'                    =>  'TRIN 1: Database',
-	                                        'Page_1_Heading'                =>  'Indstillinger Database Forbindelse',
-	                                        'Page_1_Text'                   =>  'Inden du kan anvende indstillingerne på denne side, skal du oprette en ny FULDSTÆNDIG TOM database og oprette en database bruger, der har fuld brugerrettigheder på den pågældende database.',
-	                                        
-	                                        'Database_Name'                 =>  'Database Navn:',
-	                                        'Database_Name_Default_Text'    =>  'Indsæt et Database Navn',
-	                                        'Database_Type'                 =>  'Database Type:',
-	                                        'Database_Host'                 =>  'Database Host:',
-	                                        'Database_Host_Default_Text'    =>  'Indsæt en Database Host',
-	                                        'Database_Host_Additional_Text' =>  'Dette er normalt "localhost."',
-	                                        'Table_Prefix'                  =>  'Table Prefix:',
-	                                        'Table_Prefix_Default_Text'     =>  'Indsæt et Table Prefix',
-	                                        'Table_Prefix_Additional_Text'  =>  'Kun for flere root-servere, der deler en database.',
-	                                        'Database_User'                 =>  'Database Bruger:',
-	                                        'Database_User_Default_Text'    =>  'Indsæt et Database Bruger Navn',
-	                                        'Database_PW'                   =>  'Database Password:',
-	                                        'Database_PW_Default_Text'      =>  'Indsæt et Database Password',
-	                                        'Database_PW_Additional_Text'   =>  'Opret dette som et svært, vanskelig adgangskode. Det har en masse magt, og du behøver aldrig at huske det.',
+                                            'Page_1_Tab'                    =>  'TRIN 1: Database',
+                                            'Page_1_Heading'                =>  'Indstillinger Database Forbindelse',
+                                            'Page_1_Text'                   =>  'Inden du kan anvende indstillingerne på denne side, skal du oprette en ny FULDSTÆNDIG TOM database og oprette en database bruger, der har fuld brugerrettigheder på den pågældende database.',
+                                            
+                                            'Database_Name'                 =>  'Database Navn:',
+                                            'Database_Name_Default_Text'    =>  'Indsæt et Database Navn',
+                                            'Database_Type'                 =>  'Database Type:',
+                                            'Database_Host'                 =>  'Database Host:',
+                                            'Database_Host_Default_Text'    =>  'Indsæt en Database Host',
+                                            'Database_Host_Additional_Text' =>  'Dette er normalt "localhost."',
+                                            'Table_Prefix'                  =>  'Table Prefix:',
+                                            'Table_Prefix_Default_Text'     =>  'Indsæt et Table Prefix',
+                                            'Table_Prefix_Additional_Text'  =>  'Kun for flere root-servere, der deler en database.',
+                                            'Database_User'                 =>  'Database Bruger:',
+                                            'Database_User_Default_Text'    =>  'Indsæt et Database Bruger Navn',
+                                            'Database_PW'                   =>  'Database Password:',
+                                            'Database_PW_Default_Text'      =>  'Indsæt et Database Password',
+                                            'Database_PW_Additional_Text'   =>  'Opret dette som et svært, vanskelig adgangskode. Det har en masse magt, og du behøver aldrig at huske det.',
 
-	                                        'Maps_API_Key_Warning'          =>  'ADVARSEL: Der er et problem med API-nøglen til Google Maps.',
-	                                        'Maps_API_Key_Not_Set'          =>  'ADVARSEL: Google Maps API-nøglen er ikke angivet.',
-	                                        'Maps_API_Key_Valid'            =>  'Google Maps API nøgle er gyldig.',
-	                                        
-	                                        'Page_2_Tab'                    =>  'Trin 2: Indstillinger for Google Maps API',
-	                                        'Page_2_Heading'                =>  'Indstillinger for Google Maps API',
-	                                        'Page_2_API_Key_Prompt'         =>  'Enter the Google API Key for Maps:',
-	                                        'Page_2_API_Key_Set_Button'     =>  'TESTSØGLE',
-	                                        'Page_2_API_Key_Not_Set_Prompt' =>  'SÆT API KEY FØRST',
-	                                        'Page_2_Text'                   =>  'Når du gemmer et møde, bruger BMLT Root Server API'en til Google Maps til at bestemme bredde og længdegrad for mødeadressen. Disse indstillinger er nødvendige for at tillade, at BMLT Root Server kommunikerer med Google Maps API.',
+                                            'Maps_API_Key_Warning'          =>  'ADVARSEL: Der er et problem med API-nøglen til Google Maps.',
+                                            'Maps_API_Key_Not_Set'          =>  'ADVARSEL: Google Maps API-nøglen er ikke angivet.',
+                                            'Maps_API_Key_Valid'            =>  'Google Maps API nøgle er gyldig.',
+                                            
+                                            'Page_2_Tab'                    =>  'Trin 2: Indstillinger for Google Maps API',
+                                            'Page_2_Heading'                =>  'Indstillinger for Google Maps API',
+                                            'Page_2_API_Key_Prompt'         =>  'Enter the Google API Key for Maps:',
+                                            'Page_2_API_Key_Set_Button'     =>  'TESTSØGLE',
+                                            'Page_2_API_Key_Not_Set_Prompt' =>  'SÆT API KEY FØRST',
+                                            'Page_2_Text'                   =>  'Når du gemmer et møde, bruger BMLT Root Server API en til Google Maps til at bestemme bredde og længdegrad for mødeadressen. Disse indstillinger er nødvendige for at tillade, at BMLT Root Server kommunikerer med Google Maps API.',
 
-	                                        'Page_3_Tab'                    =>  'TRIN 3: Serverindstillinger',
-	                                        'Page_3_Heading'                =>  'Indstil forskellige globale serverindstillinger',
-	                                        'Page_3_Text'                   =>  'Dette er nogle få indstillinger, der påvirker administrationen og den generelle konfiguration af denne server. De fleste serverindstillinger udføres på selve serveren.',
-	                                        'Admin_Login'                   =>  'Login til serveradministrator:',
-	                                        'Admin_Login_Default_Text'      =>  'Indsæt et serveradministrator login',
-	                                        'Admin_Login_Additional_Text'   =>  'Dette er login-streng for serveradministratoren.',
-	                                        'Admin_Password'                =>  'Server Administrator Password:',
-	                                        'Admin_Password_Default_Text'   =>  'Indsæt en serveradministratoradgangskode',
-	                                        'Admin_Password_Additional_Text'    =>  'Sørg for, at dette er et ikke-trivielt kodeord! Det har en masse betydning! (Og Glem det ikke).',
+                                            'Page_3_Tab'                    =>  'TRIN 3: Serverindstillinger',
+                                            'Page_3_Heading'                =>  'Indstil forskellige globale serverindstillinger',
+                                            'Page_3_Text'                   =>  'Dette er nogle få indstillinger, der påvirker administrationen og den generelle konfiguration af denne server. De fleste serverindstillinger udføres på selve serveren.',
+                                            'Admin_Login'                   =>  'Login til serveradministrator:',
+                                            'Admin_Login_Default_Text'      =>  'Indsæt et serveradministrator login',
+                                            'Admin_Login_Additional_Text'   =>  'Dette er login-streng for serveradministratoren.',
+                                            'Admin_Password'                =>  'Server Administrator Password:',
+                                            'Admin_Password_Default_Text'   =>  'Indsæt en serveradministratoradgangskode',
+                                            'Admin_Password_Additional_Text'    =>  'Sørg for, at dette er et ikke-trivielt kodeord! Det har en masse betydning! (Og Glem det ikke).',
                                             'ServerAdminName'               =>  'Server Administrator',
                                             'ServerAdminDesc'               =>  'Main Server Administrator',
                                             'ServerLangLabel'               =>  'Standard server sprog:',
@@ -115,12 +115,12 @@
                                             'EmailContactAllAdminEnableLabel'       =>  'Medtag alle service enhedsadministratorer på disse e-mails:',
                                             'EmailContactAllAdminEnableExtraText'   =>  'Send kopier af disse e-mails til alle relevante Service Enheds Administrators.',
                                             
-	                                        'Page_4_Tab'                    =>  'TRIN 4: Gem indstillingerne',
-	                                        'Page_4_DB_Setup_Heading'       =>  'Initialiser en ny database',
-	                                        'Page_4_DB_Setup_Text'          =>  'Knappen nedenfor vil oprette en ny, initialiseret database med standardformater, ingen service Enheder og serveradministrator bruger.',
-	                                        'Set_Up_Database'               =>  'Initialiser database',
-	                                        'Page_4_Heading'                =>  'Opret indstillingsfilen',
-	                                        'Page_4_Text'                   =>  'På grund af sikkerhedsproblemer (Ja, vi er ret paranoid -go figur), vil dette program ikke forsøge at oprette eller ændre indstillingsfilen. I stedet beder vi dig om at oprette det selv, via FTP eller en kontrolpanel filhåndtering, navngiv det "auto-config.inc.php" og indsæt følgende tekst i filen:',
+                                            'Page_4_Tab'                    =>  'TRIN 4: Gem indstillingerne',
+                                            'Page_4_DB_Setup_Heading'       =>  'Initialiser en ny database',
+                                            'Page_4_DB_Setup_Text'          =>  'Knappen nedenfor vil oprette en ny, initialiseret database med standardformater, ingen service Enheder og serveradministrator bruger.',
+                                            'Set_Up_Database'               =>  'Initialiser database',
+                                            'Page_4_Heading'                =>  'Opret indstillingsfilen',
+                                            'Page_4_Text'                   =>  'På grund af sikkerhedsproblemer (Ja, vi er ret paranoid -go figur), vil dette program ikke forsøge at oprette eller ændre indstillingsfilen. I stedet beder vi dig om at oprette det selv, via FTP eller en kontrolpanel filhåndtering, navngiv det "auto-config.inc.php" og indsæt følgende tekst i filen:',
                                             
                                             'DefaultPasswordLength'         =>  10,
                                             'DefaultMeetingCount'           =>  10,
@@ -134,9 +134,9 @@
                                             'TitleTextInitialText'          =>  'Basic Meeting List Toolbox Administration',
                                             'DefaultRegionBias'             =>  'us',
                                             'search_spec_map_center'        =>  array ( 'Længdegrad' => -118.563659, 'Højdegrad' => 34.235918, 'zoom' => 6 ),
-	                                        'DistanceChoices'               =>  array ( 2, 5, 10, 20, 50 ),
-	                                        'HistoryChoices'                =>  array ( 1, 2, 3, 5, 8, 10, 15 ),
-	                                        'PW_LengthChices'               =>  array ( 6, 8, 10, 12, 16 ),
+                                            'DistanceChoices'               =>  array ( 2, 5, 10, 20, 50 ),
+                                            'HistoryChoices'                =>  array ( 1, 2, 3, 5, 8, 10, 15 ),
+                                            'PW_LengthChices'               =>  array ( 6, 8, 10, 12, 16 ),
                                             'ServerAdminDefaultLogin'       =>  'serveradmin',
                                             
                                             'Explanatory_Text_1_Initial_Intro'  =>  'Denne installationsguiden fører dig gennem processen med oprettelse af en startdatabase samt en konfigurationsfil. I det sidste trin opretter vi en indstillingsfil og initialiserer en tom database.',
@@ -154,5 +154,4 @@
                                             'Explanatory_Text_4_File_Extra'     =>  'Du skal også sikre dig, at filtilladelserne er begrænset (chmod 0644). Dette forhindrer filen i at blive skrevet, og rodserveren kører ikke, medmindre filen har de korrekte tilladelser.',
                                             'Page_4_PathInfo'                   =>  'Filen skal placeres som %s/auto-config.inc.php, hvor din% s-mappe er. Når filen er oprettet, og du har lagt ovenstående tekst i den, skal du udføre følgende kommando for at sikre, at tilladelserne er korrekte:',
                                             'Page_4_Final'                      =>  'Når alt dette er færdig, opdatere denne side, og du bør kunne se root serveren login siden.',
-	                                        );
-?>
+                                            );
