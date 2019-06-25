b8c9b2eb811d62c0143854df2f07c2408abf9a2a
pjaudiomv <pjaudiomv@gmail.com>
2019-06-24 21:44:40 -0400

    change license from gnu to mit

--- main_server/client_interface/xml/GetLangs.php
+++ main_server/client_interface/xml/GetLangs.php
@@ -5,20 +5,18 @@
 
     This file is part of the Basic Meeting List Toolbox (BMLT).
 
-    Find out more at: http://bmlt.magshare.org
+    Find out more at: https://bmlt.app
 
     BMLT is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    it under the terms of the MIT License.
 
     BMLT is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    MIT for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this code.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the MIT along with this code.
+    If not, see <https://opensource.org/licenses/MIT>.
 ********************************************************************************************/
 
 defined('BMLT_EXEC') or define('BMLT_EXEC', true); // This is a security verifier. Keeps files from being executed outside of the context
