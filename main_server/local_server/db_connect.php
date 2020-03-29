71264fdf82674c3056c86071ef8bdf5f70c18427
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2020-03-28 18:35:06 -0700

    Remove problematic migrations that corrupted formats table when combined with 10 and 11

--- main_server/local_server/db_connect.php
+++ main_server/local_server/db_connect.php
@@ -187,74 +187,13 @@ function DB_Connect_and_Upgrade()
             c_comdef_dbsingleton::preparedExec($updateSql);
         }),
         array(5, function () {
-            $dbPrefix = $GLOBALS['dbPrefix'];
-            $table = "$dbPrefix" . "_comdef_formats";
-            $check_column_sql = "SELECT COUNT(*) AS count FROM `$table` WHERE `lang_enum`='fa'";
-            $rows = c_comdef_dbsingleton::preparedQuery($check_column_sql);
-            if (is_array($rows) && count($rows)) {
-                $row = $rows[0];
-                if (intval($row['count']) == 0) {
-                    // The SEZF db was latin1 for some reason, so let's just be safe and convert to utf8
-                    $sql = "ALTER TABLE `" . $table . "` CONVERT TO CHARACTER SET utf8";
-                    c_comdef_dbsingleton::preparedExec($sql);
-
-                    $sql_temp = str_replace('%%PREFIX%%', preg_replace('|[^a-z_\.\-A-Z0-9]|', '', $dbPrefix), file_get_contents(dirname(__FILE__)."/install_wizard/sql_files/InitialFormatsData-fa.sql"));
-                    $value_array = array();
-                    $sql_temp = str_replace("\\'", "`", $sql_temp);
-                    preg_match_all("|'(.*?)'|", $sql_temp, $value_array);
-                    $value_array = $value_array[0];
-                    for ($c = 0; $c < count($value_array); $c++) {
-                        $value_array[$c] = preg_replace("|'(.*?)'|", "$1", $value_array[$c]);
-                        $value_array[$c] = str_replace("`", "'", $value_array[$c]);
-                    }
-                    $sql_temp = preg_replace("|'.*?'|", "?", $sql_temp);
-                    c_comdef_dbsingleton::preparedExec($sql_temp, $value_array);
-                }
-            }
+            return;
         }),
         array(6, function () {
-            $dbPrefix = $GLOBALS['dbPrefix'];
-            $table = "$dbPrefix" . "_comdef_formats";
-            $check_column_sql = "SELECT COUNT(*) AS count FROM `$table` WHERE `lang_enum`='pl'";
-            $rows = c_comdef_dbsingleton::preparedQuery($check_column_sql);
-            if (is_array($rows) && count($rows)) {
-                $row = $rows[0];
-                if (intval($row['count']) == 0) {
-                    $sql_temp = str_replace('%%PREFIX%%', preg_replace('|[^a-z_\.\-A-Z0-9]|', '', $dbPrefix), file_get_contents(dirname(__FILE__)."/install_wizard/sql_files/InitialFormatsData-pl.sql"));
-                    $value_array = array();
-                    $sql_temp = str_replace("\\'", "`", $sql_temp);
-                    preg_match_all("|'(.*?)'|", $sql_temp, $value_array);
-                    $value_array = $value_array[0];
-                    for ($c = 0; $c < count($value_array); $c++) {
-                        $value_array[$c] = preg_replace("|'(.*?)'|", "$1", $value_array[$c]);
-                        $value_array[$c] = str_replace("`", "'", $value_array[$c]);
-                    }
-                    $sql_temp = preg_replace("|'.*?'|", "?", $sql_temp);
-                    c_comdef_dbsingleton::preparedExec($sql_temp, $value_array);
-                }
-            }
+            return;
         }),
         array(7, function () {
-            $dbPrefix = $GLOBALS['dbPrefix'];
-            $table = "$dbPrefix" . "_comdef_formats";
-            $check_column_sql = "SELECT COUNT(*) AS count FROM `$table` WHERE `lang_enum`='pt'";
-            $rows = c_comdef_dbsingleton::preparedQuery($check_column_sql);
-            if (is_array($rows) && count($rows)) {
-                $row = $rows[0];
-                if (intval($row['count']) == 0) {
-                    $sql_temp = str_replace('%%PREFIX%%', preg_replace('|[^a-z_\.\-A-Z0-9]|', '', $dbPrefix), file_get_contents(dirname(__FILE__)."/install_wizard/sql_files/InitialFormatsData-pt.sql"));
-                    $value_array = array();
-                    $sql_temp = str_replace("\\'", "`", $sql_temp);
-                    preg_match_all("|'(.*?)'|", $sql_temp, $value_array);
-                    $value_array = $value_array[0];
-                    for ($c = 0; $c < count($value_array); $c++) {
-                        $value_array[$c] = preg_replace("|'(.*?)'|", "$1", $value_array[$c]);
-                        $value_array[$c] = str_replace("`", "'", $value_array[$c]);
-                    }
-                    $sql_temp = preg_replace("|'.*?'|", "?", $sql_temp);
-                    c_comdef_dbsingleton::preparedExec($sql_temp, $value_array);
-                }
-            }
+            return;
         }),
         array(8, function () {
             $dbPrefix = $GLOBALS['dbPrefix'];
