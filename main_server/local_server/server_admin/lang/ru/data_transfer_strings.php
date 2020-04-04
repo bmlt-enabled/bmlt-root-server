00f1988c3e608cc108c5d7aa1bb0313335937390
pjaudiomv <pjaudiomv@gmail.com>
2020-04-04 09:01:24 -0400

    adding Russian RU

---
+++ main_server/local_server/server_admin/lang/ru/data_transfer_strings.php
@@ -0,0 +1,45 @@
+<?php
+/***********************************************************************/
+/** \file   comdef_data_transfer_strings.inc.php
+    \brief  The strings displayed in the search results for this language (Russian)
+
+    This file is part of the Basic Meeting List Toolbox (BMLT).
+
+    Find out more at: https://bmlt.app
+
+    BMLT is free software: you can redistribute it and/or modify
+    it under the terms of the MIT License.
+
+    BMLT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MIT License for more details.
+
+    You should have received a copy of the MIT License along with this code.
+    If not, see <https://opensource.org/licenses/MIT>.*/
+    defined('BMLT_EXEC') or die('Cannot Execute Directly');    // Makes sure that this file is in the correct context.
+    
+    $comdef_data_transfer_strings = array ( 'key_strings'   =>  array ( 'id_bigint' => 1,
+                                                                        'worldid_mixed' => 1,
+                                                                        'shared_group_id_bigint' => 1,
+                                                                        'service_body_bigint' => 1,
+                                                                        'weekday_tinyint' => 1,
+                                                                        'start_time' => 1,
+                                                                        'duration_time' => 1,
+                                                                        'formats' => 1,
+                                                                        'lang_enum' => 1,
+                                                                        'longitude' => 1,
+                                                                        'latitude' => 1,
+                                                                        'published' => 1,
+                                                                        'email_contact' => 1
+                                                                        ),
+                                                                        
+                                            'days'          =>  array ( 'Воскресенье',
+                                                                        'Понедельник',
+                                                                        'Вторник',
+                                                                        'Среда',
+                                                                        'Четверг',
+                                                                        'Пятница',
+                                                                        'Суббота'
+                                                                        ),
+                                            );
