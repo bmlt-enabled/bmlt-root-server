2f9dd0e46952c8626fbccecbcf0fac42b7c111d0
Danny <danny.gershman@gmail.com>
2020-07-12 21:57:27 -0400

    allow dynamic loading of themes

--- main_server/client_interface/html/index.php
+++ main_server/client_interface/html/index.php
@@ -56,7 +56,7 @@ if ($user_obj instanceof c_comdef_user && $user_obj->GetUserLevel() != _USER_LEV
                 root_server: "<?php echo str_replace('client_interface/html/', '', GetURLToMainServerDirectory()) ?>",
                 service_body: <?php echo json_encode($service_body_ids) ?>,
                 template_path: "croutonjs/templates",
-                theme: "florida-nights",
+                theme: "<?php echo (isset($_GET["theme"]) ? html_entity_decode($_GET["theme"]) : "florida-nights") ?>",
                 has_languages: "1",
                 has_areas: "1",
                 time_format: "H:mm (h:mma) z",
