91fa8e44edc091e57222072fb9984d811f056639
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2020-10-19 10:38:12 -0400

    attempt to handle more cases, plus international

--- main_server/server/classes/time_zone_migrator.class.php
+++ main_server/server/classes/time_zone_migrator.class.php
@@ -4,29 +4,153 @@ defined('BMLT_EXEC') or die('Cannot Execute Directly');    // Makes sure that th
 // phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
 class TimeZoneMigrator
 {
+    private $multi_time_zone_states = array(
+        "oregon",
+        "or",
+        "idaho",
+        "id",
+        "nebraska",
+        "ne",
+        "kansas",
+        "ks",
+        "texas",
+        "tx",
+        "north dakota",
+        "nd",
+        "south dakota",
+        "sd",
+        "florida",
+        "fl",
+        "michigan",
+        "mi",
+        "indiana",
+        "in",
+        "kentucky",
+        "ky",
+        "tennessee",
+        "tn",
+        "alaska",
+        "ak"
+    );
+
     // phpcs:enable PSR1.Classes.ClassDeclaration.MissingNamespace
     public function migrateOne()
     {
         try {
-            $city_and_state =$this->getNextCityAndState();
-            if (!$city_and_state) {
-                return;
+            $location = $this->getNext();
+            if (!$location) {
+                return false;
             }
-            $city = $city_and_state[0];
-            $state = $city_and_state[1];
 
-            $time_zone = $this->getTimeZone($city, $state);
+            $city = $location[0];
+            $zip_code = $location[1];
+            $county = $location[2];
+            $state = $location[3];
+            $country = $location[4];
+            if ($city && $state) {
+                $location = "$city, $state";
+                if ($country) {
+                    $location .= ", $country";
+                }
+            } else {
+                $location = "";
+                // If city is here we will use it to help with geocoding, but
+                // the update will be by country plus county/state/zip
+                if ($city) {
+                    $location = $city;
+                }
+
+                if ($county) {
+                    $location .= ($location ? ", $county" : $county);
+                }
+
+                if ($state) {
+                    $location .= ($location ? ", $state" : $state);
+                }
+
+                if ($zip_code) {
+                    $location .= ($location ? ", $zip_code" : $zip_code);
+                }
+
+                if ($country) {
+                    $location .= ($location ? ", $country" : $country);
+                }
+            }
+
+
+            $time_zone = $this->getTimeZone($location);
             if (!$time_zone) {
                 return;
             }
 
-            $this->updateTimeZone($city, $state, $time_zone);
+            if ($city && $state) {
+                $this->updateTimeZoneByCityAndState($city, $state, $time_zone);
+            } else {
+                $this->updateTimeZoneByCountryAndZipOrStateOrCounty($zip_code, $county, $state, $country, $time_zone);
+            }
+
         } catch (Exception $e) {
             // do nothing
         }
     }
 
-    private function updateTimeZone($city, $state, $time_zone)
+    private function updateTimeZoneByCountryAndZipOrStateOrCounty($zip_code, $county, $state, $country, $time_zone)
+    {
+        $meetings_main = c_comdef_server::GetMeetingTableName_obj() . "_main";
+        $meetings_data = c_comdef_server::GetMeetingTableName_obj() . "_data";
+
+        if (!$country) {
+            return;
+        }
+        if (!$zip_code && !$county && !$state) {
+            return;
+        }
+
+        $conditionTemplate = "    AND\n";
+        $conditionTemplate .= "    EXISTS (\n";
+        $conditionTemplate .= "        SELECT 1\n";
+        $conditionTemplate .= "        FROM $meetings_data\n";
+        $conditionTemplate .= "        WHERE\n";
+        $conditionTemplate .= "            $meetings_data.meetingid_bigint = $meetings_main.id_bigint\n";
+        $conditionTemplate .= "            AND\n";
+        $conditionTemplate .= "            $meetings_data.key = ?\n";
+        $conditionTemplate .= "            AND\n";
+        $conditionTemplate .= "            $meetings_data.data_string = ?\n";
+        $conditionTemplate .= "    )\n";
+
+        $sql = "UPDATE $meetings_main\n";
+        $sql .= "SET $meetings_main.time_zone = ?\n";
+        $sql .= "WHERE\n";
+        $sql .= "    EXISTS (\n";
+        $sql .= "        SELECT 1\n";
+        $sql .= "        FROM $meetings_data\n";
+        $sql .= "        WHERE\n";
+        $sql .= "            $meetings_data.meetingid_bigint = $meetings_main.id_bigint\n";
+        $sql .= "            AND\n";
+        $sql .= "            $meetings_data.key = 'location_nation'\n";
+        $sql .= "            AND\n";
+        $sql .= "            $meetings_data.data_string = ?\n";
+        $sql .= "    )\n";
+
+        $params = array($time_zone, $country);
+        if ($zip_code) {
+            $sql .= $conditionTemplate;
+            array_push($params, "location_postal_code_1", $zip_code);
+        }
+        if ($county) {
+            $sql .= $conditionTemplate;
+            array_push($params, "location_sub_province", $county);
+        }
+        if ($state) {
+            $sql .= $conditionTemplate;
+            array_push($params, "location_province", $state);
+        }
+
+        c_comdef_dbsingleton::preparedExec($sql, $params);
+
+    }
+
+    private function updateTimeZoneByCityAndState($city, $state, $time_zone)
     {
         $meetings_main = c_comdef_server::GetMeetingTableName_obj() . "_main";
         $meetings_data = c_comdef_server::GetMeetingTableName_obj() . "_data";
@@ -61,11 +185,11 @@ class TimeZoneMigrator
         c_comdef_dbsingleton::preparedExec($sql, $params);
     }
 
-    private function getTimeZone($city, $state)
+    private function getTimeZone($location)
     {
         // little bit obfuscate
         $url = base64_decode("aHR0cHM6Ly91YWE4cmRjaGcyLmV4ZWN1dGUtYXBpLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tL3Rlc3QvYm1sdC8=");
-        $url .= "?location=$city,$state";
+        $url .= "?location=$location";
         $xml = simplexml_load_file($url);
         if ($xml->status == 'OK') {
             $time_zone = strval($xml->time_zone_id);
@@ -74,28 +198,51 @@ class TimeZoneMigrator
         return false;
     }
 
-    private function getNextCityAndState()
+    private function getNext()
     {
         $meetings_main = c_comdef_server::GetMeetingTableName_obj() . "_main";
         $meetings_data = c_comdef_server::GetMeetingTableName_obj() . "_data";
-
-        $sql =  "SELECT location_municipality, location_province FROM (\n";
+        
+        $sql = "SELECT city, zip_code, county, state, country FROM (\n";
         $sql .= "    SELECT DISTINCT\n";
-        $sql .= "        (SELECT data_string FROM $meetings_data WHERE $meetings_data.meetingid_bigint = $meetings_main.id_bigint AND $meetings_data.key = 'location_municipality') as location_municipality,\n";
-        $sql .= "        (SELECT data_string FROM $meetings_data WHERE $meetings_data.meetingid_bigint = $meetings_main.id_bigint AND $meetings_data.key = 'location_province') as location_province\n";
+        $sql .= "        (SELECT data_string FROM $meetings_data WHERE $meetings_data.meetingid_bigint = $meetings_main.id_bigint AND $meetings_data.key = 'location_municipality') as city,\n";
+        $sql .= "        (SELECT data_string FROM $meetings_data WHERE $meetings_data.meetingid_bigint = $meetings_main.id_bigint AND $meetings_data.key = 'location_postal_code_1') as zip_code,\n";
+        $sql .= "        (SELECT data_string FROM $meetings_data WHERE $meetings_data.meetingid_bigint = $meetings_main.id_bigint AND $meetings_data.key = 'location_sub_province') as county,\n";
+        $sql .= "        (SELECT data_string FROM $meetings_data WHERE $meetings_data.meetingid_bigint = $meetings_main.id_bigint AND $meetings_data.key = 'location_province') as state,\n";
+        $sql .= "        (SELECT data_string FROM $meetings_data WHERE $meetings_data.meetingid_bigint = $meetings_main.id_bigint AND $meetings_data.key = 'location_nation') as country\n";
         $sql .= "    FROM $meetings_main\n";
         $sql .= "    WHERE $meetings_main.time_zone IS NULL OR $meetings_main.time_zone = \"\"\n";
         $sql .= ") blah\n";
-        $sql .= "WHERE location_municipality IS NOT NULL AND location_province IS NOT NULL\n";
-        $sql .= "LIMIT 1";
+        $sql .= "WHERE\n";
+        $sql .= "    (city IS NOT NULL AND state IS NOT NULL)\n";
+        $sql .= "    OR\n";
+        $sql .= "    (country IS NOT NULL AND (zip_code IS NOT NULL OR state IS NOT NULL OR county IS NOT NULL))\n";
 
         $result = c_comdef_dbsingleton::preparedQuery($sql);
 
         if (is_array($result) && count($result)) {
-            $result = $result[0];
-            $city = $result['location_municipality'];
-            $state = $result['location_province'];
-            return array($city, $state);
+            foreach ($result as $row) {
+                $city = $row['city'];
+                $zip_code = $row['zip_code'];
+                $county = $row['county'];
+                $state = $row['state'];
+                $country = $row['country'];
+
+                // if city is not set, the country is us or usa, the state has multiple time zones,
+                // and the county and zip code are not set... skip. basically, for multi-time zone
+                // us states, don't make a decision based on state and country alone
+                if (!$city && $state) {
+                    if (strtolower($country) == "us" || strtolower($country) == "usa" || strtolower($country) == "united states") {
+                        if (in_array(strtolower($state), $this->multi_time_zone_states)) {
+                            if (!$county && !$zip_code) {
+                                continue;
+                            }
+                        }
+                    }
+                }
+
+                return array($city, $zip_code, $county, $state, $country);
+            }
         }
 
         return false;
