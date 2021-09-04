ea6350a6d7c76a3e5ed41941e480e243b91ab942
Patrick Joyce <pjaudiomv@gmail.com>
2021-09-04 19:28:55 -0400

    mv route 53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,30 +1,28 @@
-data "aws_route53_zone" "bmlt" {
-  provider = aws.bmlt
-  name     = "aws.bmlt.app."
+resource "aws_route53_zone" "bmlt" {
+  name = "aws.bmlt.app"
 }
 
-//resource "aws_route53_record" "bmlt_latest" {
-//  provider = aws.bmlt
-//  zone_id = data.aws_route53_zone.bmlt.id
-//  name    = "latest.${data.aws_route53_zone.bmlt.name}"
-//  type    = "A"
-//
-//  alias {
-//    name                   = aws_alb.bmlt.dns_name
-//    zone_id                = aws_alb.bmlt.zone_id
-//    evaluate_target_health = true
-//  }
-//}
-//
-//resource "aws_route53_record" "bmlt_unstable" {
-//  provider = aws.bmlt
-//  zone_id = data.aws_route53_zone.bmlt.id
-//  name    = "unstable.${data.aws_route53_zone.bmlt.name}"
-//  type    = "A"
-//
-//  alias {
-//    name                   = aws_alb.bmlt.dns_name
-//    zone_id                = aws_alb.bmlt.zone_id
-//    evaluate_target_health = true
-//  }
-//}
+resource "aws_route53_record" "bmlt_latest" {
+  zone_id = aws_route53_zone.bmlt.id
+  name    = "latest.${aws_route53_zone.bmlt.name}"
+  type    = "A"
+
+
+  alias {
+    name                   = data.aws_lb.main.dns_name
+    zone_id                = data.aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "bmlt_unstable" {
+  zone_id = aws_route53_zone.bmlt.id
+  name    = "unstable.${aws_route53_zone.bmlt.name}"
+  type    = "A"
+
+  alias {
+    name                   = data.aws_lb.main.dns_name
+    zone_id                = data.aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
