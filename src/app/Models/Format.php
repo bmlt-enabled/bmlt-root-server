37b20c3a4b52d5ea029849ae87d27f53a1718615
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-08-31 16:19:53 -0400

    add Formats model

---
+++ src/app/Models/Format.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Format extends Model
+{
+    protected $table = 'comdef_formats';
+    public $timestamps = false;
+}
