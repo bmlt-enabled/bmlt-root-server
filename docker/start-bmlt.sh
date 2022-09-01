e478b85b52b5afb926d6f2fafe1f13f8aa65b2d5
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-09-01 14:14:00 -0400

    run tests in ci, rework ci file a bit

--- docker/start-bmlt.sh
+++ docker/start-bmlt.sh
@@ -1,44 +1,44 @@
-sed -i '/gkey/d' /var/www/auto-config.inc.php
-sed -i '/dbName/d' /var/www/auto-config.inc.php
-sed -i '/dbUser/d' /var/www/auto-config.inc.php
-sed -i '/dbPassword/d' /var/www/auto-config.inc.php
-sed -i '/dbServer/d' /var/www/auto-config.inc.php
-sed -i '/dbPrefix/d' /var/www/auto-config.inc.php
-sed -i '/do_not_force_port/d' /var/www/auto-config.inc.php
+sed -i '/gkey/d' /var/www/html/auto-config.inc.php
+sed -i '/dbName/d' /var/www/html/auto-config.inc.php
+sed -i '/dbUser/d' /var/www/html/auto-config.inc.php
+sed -i '/dbPassword/d' /var/www/html/auto-config.inc.php
+sed -i '/dbServer/d' /var/www/html/auto-config.inc.php
+sed -i '/dbPrefix/d' /var/www/html/auto-config.inc.php
+sed -i '/do_not_force_port/d' /var/www/html/auto-config.inc.php
 
 if [ ! -z $GKEY ]
 then
-    echo "\$gkey = \"$GKEY\";" >> /var/www/auto-config.inc.php
+    echo "\$gkey = \"$GKEY\";" >> /var/www/html/auto-config.inc.php
 fi
 
 if [ ! -z $DBNAME ]
 then
-    echo "\$dbName = \"$DBNAME\";" >> /var/www/auto-config.inc.php
+    echo "\$dbName = \"$DBNAME\";" >> /var/www/html/auto-config.inc.php
 fi
 
 if [ ! -z $DBUSER ]
 then
-    echo "\$dbUser = \"$DBUSER\";" >> /var/www/auto-config.inc.php
+    echo "\$dbUser = \"$DBUSER\";" >> /var/www/html/auto-config.inc.php
 fi
 
 if [ ! -z $DBPASSWORD ]
 then
-    echo "\$dbPassword = \"$DBPASSWORD\";" >> /var/www/auto-config.inc.php
+    echo "\$dbPassword = \"$DBPASSWORD\";" >> /var/www/html/auto-config.inc.php
 fi
 
 if [ ! -z $DBSERVER ]
 then
-    echo "\$dbServer = \"$DBSERVER\";" >> /var/www/auto-config.inc.php
+    echo "\$dbServer = \"$DBSERVER\";" >> /var/www/html/auto-config.inc.php
 fi
 
 if [ ! -z $DBPREFIX ]
 then
-    echo "\$dbPrefix = \"$DBPREFIX\";" >> /var/www/auto-config.inc.php
+    echo "\$dbPrefix = \"$DBPREFIX\";" >> /var/www/html/auto-config.inc.php
 fi
 
 if [ ! -z "$DO_NOT_FORCE_PORT" -a "$DO_NOT_FORCE_PORT" == "true" ]
 then
-    echo "\$g_do_not_force_port = true;" >> /var/www/auto-config.inc.php
+    echo "\$g_do_not_force_port = true;" >> /var/www/html/auto-config.inc.php
 fi
 
 apachectl -D FOREGROUND
