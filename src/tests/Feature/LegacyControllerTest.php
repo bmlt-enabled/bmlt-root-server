d7caf4b004a7034ca20a2b46a42badfcbb014931
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-09-01 14:14:00 -0400

    run tests in ci, rework ci file a bit

--- src/tests/Feature/LegacyControllerTest.php
+++ src/tests/Feature/LegacyControllerTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class LegacyControllerTest extends TestCase
 {
     /**
      * A basic test example.
@@ -16,6 +16,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(500);
     }
 }
