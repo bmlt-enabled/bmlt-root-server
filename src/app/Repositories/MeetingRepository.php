a0adaa65c488585c3dba93b1c36d18fe4b787da7
jbraswell <10187286+jbraswell@users.noreply.github.com>
2022-09-07 13:33:03 -0400

    collapse a couple repositories into meetingRepository (#555)

--- src/app/Repositories/MeetingRepository.php
+++ src/app/Repositories/MeetingRepository.php
@@ -2,12 +2,14 @@
 
 namespace App\Repositories;
 
-use App\Interfaces\FieldValuesRepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
+use App\Interfaces\MeetingRepositoryInterface;
 use App\Models\Meeting;
 use App\Models\MeetingData;
-use Illuminate\Support\Collection;
 
-class FieldValuesRepository implements FieldValuesRepositoryInterface
+class MeetingRepository implements MeetingRepositoryInterface
 {
     private static array $mainFields = [
         'id_bigint',
@@ -24,9 +26,63 @@ class FieldValuesRepository implements FieldValuesRepositoryInterface
         'latitude',
     ];
 
+    public function getFieldKeys(): Collection
+    {
+        $data = [
+            ['key' => 'id_bigint', 'description' => __('main_prompts.id_bigint')],
+            ['key' => 'worldid_mixed', 'description' => __('main_prompts.worldid_mixed')],
+            ['key' => 'service_body_bigint', 'description' => __('main_prompts.service_body_bigint')],
+            ['key' => 'weekday_tinyint', 'description' => __('main_prompts.weekday_tinyint')],
+            ['key' => 'venue_type', 'description' => __('main_prompts.venue_type')],
+            ['key' => 'start_time', 'description' => __('main_prompts.start_time')],
+            ['key' => 'duration_time', 'description' => __('main_prompts.duration_time')],
+            ['key' => 'time_zone', 'description' => __('main_prompts.time_zone')],
+            ['key' => 'formats', 'description' => __('main_prompts.formats')],
+            ['key' => 'lang_enum', 'description' => __('main_prompts.lang_enum')],
+            ['key' => 'longitude', 'description' => __('main_prompts.longitude')],
+            ['key' => 'latitude', 'description' => __('main_prompts.latitude')],
+        ];
+
+        $langEnum = App::currentLocale();
+        $fields = MeetingData::query()
+            ->where('meetingid_bigint', 0)
+            ->where('lang_enum', $langEnum)
+            ->where(function (Builder $query) {
+                $query->where('visibility', null)->orWhereNot('visibility', 1);
+            })
+            ->get();
+
+        foreach ($fields as $field) {
+            array_push($data, ['key' => $field->key, 'description' => $field->field_prompt]);
+        }
+
+        if ($langEnum != 'en') {
+            $seenKeys = [];
+            foreach ($data as $f) {
+                $seenKeys[$f['key']] = null;
+            }
+
+            $fields = MeetingData::query()
+                ->where('meetingid_bigint', 0)
+                ->where('lang_enum', 'en')
+                ->where(function ($query) {
+                    $query->where('visibility', null)->orWhereNot('visibility', 1);
+                })
+                ->get();
+
+            foreach ($fields as $field) {
+                if (!array_key_exists($field->key, $seenKeys)) {
+                    array_push($data, ['key' => $field->key, 'description' => $field->field_prompt]);
+                }
+            }
+        }
+
+        return collect($data);
+    }
+
     public function getFieldValues(string $fieldName, array $specificFormats = [], bool $allFormats = false): Collection
     {
-        if (in_array($fieldName, FieldValuesRepository::$mainFields)) {
+        if (in_array($fieldName, self::$mainFields)) {
             $meetingIdsByValue = Meeting::all()
                 ->mapToGroups(function ($meeting, $key) use ($fieldName, $specificFormats, $allFormats) {
                     $value = $meeting->{$fieldName};
