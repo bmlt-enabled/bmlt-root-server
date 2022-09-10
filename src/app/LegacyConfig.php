5d4de11065ca2295713f3e8058ca51e5792efb3a
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-09-10 17:14:28 -0400

    rewrite GetServerInfo

--- src/app/LegacyConfig.php
+++ src/app/LegacyConfig.php
@@ -24,6 +24,33 @@ class LegacyConfig
         return self::$config[$key] ?? $default;
     }
 
+    public static function set(string $key, $value)
+    {
+        // really should only be used in testing
+        if (!self::$configLoaded) {
+            self::loadConfig();
+        }
+
+        self::$config[$key] = $value;
+    }
+
+    public static function remove(string $key)
+    {
+        // really should only be used in testing
+        if (!self::$configLoaded) {
+            self::loadConfig();
+        }
+
+        unset(self::$config[$key]);
+    }
+
+    public static function reset()
+    {
+        // really should only be used in testing
+        self::$config = null;
+        self::$configLoaded = false;
+    }
+
     private static function loadConfig()
     {
         $legacyConfigFile = base_path() . '/../auto-config.inc.php';
@@ -63,6 +90,18 @@ class LegacyConfig
         if (isset($comdef_global_language)) {
             $config['language'] = $comdef_global_language;
         }
+
+        $config['default_duration_time'] = $default_duration_time ?? '01:00:00';
+        $config['region_bias'] = $region_bias ?? 'us';
+        $config['distance_units'] = $comdef_distance_units ?? 'mi';
+        $config['enable_semantic_admin'] = isset($g_enable_semantic_admin) && $g_enable_semantic_admin;
+        $config['enable_email_contact'] = isset($g_enable_email_contact) && $g_enable_email_contact;
+        $config['include_service_body_admin_on_emails'] = isset($include_service_body_admin_on_emails) && $include_service_body_admin_on_emails;
+        $config['change_depth_for_meetings'] = $change_depth_for_meetings ?? 0;
+        $config['meeting_states_and_provinces'] = $meeting_states_and_provinces ?? [];
+        $config['meeting_counties_and_sub_provinces'] = $meeting_counties_and_sub_provinces ?? [];
+        $config['meeting_time_zones_enabled'] = isset($meeting_time_zones_enabled) && $meeting_time_zones_enabled;
+
 
         self::$config = $config;
         self::$configLoaded = true;
