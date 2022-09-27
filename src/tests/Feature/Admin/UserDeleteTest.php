fae58cd18ff981d905d37dcf501ee65f2278760f
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-09-27 16:08:37 -0400

    test to validate owned users are reassigned when parent is deleted

--- src/tests/Feature/Admin/UserDeleteTest.php
+++ src/tests/Feature/Admin/UserDeleteTest.php
@@ -22,4 +22,27 @@ class UserDeleteTest extends TestCase
 
         $this->assertFalse(User::query()->where('id_bigint', $user2->id_bigint)->exists());
     }
+
+    public function testDeleteUserReassignsOrphans()
+    {
+        $user = $this->createAdminUser();
+        $token = $user->createToken('test')->plainTextToken;
+
+        $user2 = $this->createServiceBodyAdminUser();
+        $user2->owner_id_bigint = $user->id_bigint;
+        $user2->save();
+        $user3 = $this->createServiceBodyObserverUser();
+        $user3->owner_id_bigint = $user2->id_bigint;
+        $user3->save();
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->delete("/api/v1/users/$user->id_bigint")
+            ->assertStatus(204);
+
+        $user2->refresh();
+        $this->assertEquals(-1, $user2->owner_id_bigint);
+
+        $user3->refresh();
+        $this->assertEquals($user2->id_bigint, $user3->owner_id_bigint);
+    }
 }
