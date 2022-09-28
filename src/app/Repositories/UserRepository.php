99fae9fbc3404204e4da162bf15d57fe12a1d1ee
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-09-27 22:14:33 -0400

    insert change objects when users are created/updated/deleted

--- src/app/Repositories/UserRepository.php
+++ src/app/Repositories/UserRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repositories;
 
 use App\Interfaces\UserRepositoryInterface;
+use App\Models\Change;
 use App\Models\User;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 
 class UserRepository implements UserRepositoryInterface
@@ -32,7 +34,7 @@ class UserRepository implements UserRepositoryInterface
     {
         return DB::transaction(function () use ($values) {
             $user = User::create($values);
-            //$this->saveChange(null, $user);
+            $this->saveChange(null, $user);
             return $user;
         });
     }
@@ -43,7 +45,7 @@ class UserRepository implements UserRepositoryInterface
             $user = User::find($id);
             if (!is_null($user)) {
                 $user::query()->where('id_bigint', $id)->update($values);
-                // $this->saveChange($user, User::find($id));
+                $this->saveChange($user, User::find($id));
                 return true;
             }
             return false;
@@ -57,10 +59,43 @@ class UserRepository implements UserRepositoryInterface
             if (!is_null($user)) {
                 $user->children()->update(['owner_id_bigint' => -1]);
                 $user->delete();
-                // $this->saveChange($user, null);
+                $this->saveChange($user, null);
                 return true;
             }
             return false;
         });
+    }
+
+    private function saveChange(?User $beforeUser, ?User $afterUser): void
+    {
+        Change::create([
+            'user_id_bigint' => request()->user()->id_bigint,
+            'service_body_id_bigint' => $afterUser?->id_bigint ?? $beforeUser->id_bigint,
+            'lang_enum' => $beforeUser?->lang_enum ?: $afterUser?->lang_enum ?: legacy_config('language') ?: App::currentLocale(),
+            'object_class_string' => 'c_comdef_user',
+            'before_id_bigint' => $beforeUser?->id_bigint,
+            'before_lang_enum' => !is_null($beforeUser) ? $beforeUser?->lang_enum ?: legacy_config('language') ?: App::currentLocale() : null,
+            'after_id_bigint' => $afterUser?->id_bigint,
+            'after_lang_enum' => !is_null($afterUser) ? $afterUser?->lang_enum ?: legacy_config('language') ?: App::currentLocale() : null,
+            'change_type_enum' => is_null($beforeUser) ? 'comdef_change_type_new' : (is_null($afterUser) ? 'comdef_change_type_delete' : 'comdef_change_type_change'),
+            'before_object' => !is_null($beforeUser) ? $this->serializeForChange($beforeUser) : null,
+            'after_object' => !is_null($afterUser) ? $this->serializeForChange($afterUser) : null,
+        ]);
+    }
+
+    private function serializeForChange(User $user): string
+    {
+        return serialize([
+            $user->id_bigint,
+            $user->user_level_tinyint,
+            $user->email_address_string,
+            $user->login_string,
+            $user->password_string,
+            $user->last_access_datetime,
+            $user->name_string,
+            $user->description_string,
+            $user->owner_id_bigint,
+            $user->lang_enum,
+        ]);
     }
 }
---
+++ src/tests/Feature/Admin/UserChangeTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Change;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\App;
+
+class UserChangeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testNewUserChange()
+    {
+        $user = $this->createAdminUser();
+        $token = $user->createToken('test')->plainTextToken;
+        $data = [
+            'username' => 'test',
+            'password' => 'this is a valid password',
+            'type' => User::USER_TYPE_ADMIN,
+            'displayName' => 'pretty name',
+            'description' => 'test description',
+            'email' => 'test@test.com',
+            'ownerId' => $user->id_bigint,
+        ];
+
+        $newUser = $this
+            ->withHeader('Authorization', "Bearer $token")
+            ->post('/api/v1/users', $data)
+            ->json();
+
+        $change = Change::query()->first();
+        $this->assertEquals($user->id_bigint, $change->user_id_bigint);
+        $this->assertEquals($newUser['id'], $change->service_body_id_bigint);
+        $this->assertEquals(App::currentLocale(), $change->lang_enum);
+        $this->assertEquals('c_comdef_user', $change->object_class_string);
+        $this->assertNull($change->before_id_bigint);
+        $this->assertNull($change->before_lang_enum);
+        $this->assertEquals($newUser['id'], $change->after_id_bigint);
+        $this->assertEquals(App::currentLocale(), $change->after_lang_enum);
+        $this->assertEquals('comdef_change_type_new', $change->change_type_enum);
+        $this->assertNull($change->before_object);
+        $this->assertNotNull($change->after_object);
+    }
+
+    public function testChangedUserChange()
+    {
+        $user = $this->createAdminUser();
+        $token = $user->createToken('test')->plainTextToken;
+        $data = [
+            'username' => 'test',
+            'password' => 'this is a valid password',
+            'type' => User::USER_TYPE_ADMIN,
+            'displayName' => 'pretty name',
+            'description' => 'test description',
+            'email' => 'test@test.com',
+            'ownerId' => $user->id_bigint,
+        ];
+
+        $this
+            ->withHeader('Authorization', "Bearer $token")
+            ->put("/api/v1/users/$user->id_bigint", $data)
+            ->assertStatus(204);
+
+        $change = Change::query()->first();
+        $this->assertEquals($user->id_bigint, $change->user_id_bigint);
+        $this->assertEquals($user->id_bigint, $change->service_body_id_bigint);
+        $this->assertEquals(App::currentLocale(), $change->lang_enum);
+        $this->assertEquals('c_comdef_user', $change->object_class_string);
+        $this->assertEquals($user->id_bigint, $change->before_id_bigint);
+        $this->assertEquals(App::currentLocale(), $change->before_lang_enum);
+        $this->assertEquals($user->id_bigint, $change->after_id_bigint);
+        $this->assertEquals(App::currentLocale(), $change->after_lang_enum);
+        $this->assertEquals('comdef_change_type_change', $change->change_type_enum);
+        $this->assertNotNull($change->before_object);
+        $this->assertNotNull($change->after_object);
+
+        $object = $change->before_object;
+        $this->assertEquals($user->id_bigint, $object[0]);
+        $this->assertEquals($user->user_level_tinyint, $object[1]);
+        $this->assertEquals($user->email_address_string, $object[2]);
+        $this->assertEquals($user->login_string, $object[3]);
+        $this->assertEquals($user->password_string, $object[4]);
+        $this->assertEquals($user->name_string, $object[6]);
+        $this->assertEquals($user->description_string, $object[7]);
+        $this->assertEquals(-1, $object[8]);
+        $this->assertEquals(App::currentLocale(), $object[9]);
+        $object = $change->after_object;
+        $this->assertEquals($user->id_bigint, $object[0]);
+        $this->assertEquals(User::USER_TYPE_TO_USER_LEVEL_MAP[$data['type']], $object[1]);
+        $this->assertEquals($data['email'], $object[2]);
+        $this->assertEquals($data['username'], $object[3]);
+        $this->assertNotEquals($user->password_string, $object[4]);
+        $this->assertEquals($data['displayName'], $object[6]);
+        $this->assertEquals($data['description'], $object[7]);
+        $this->assertEquals($data['ownerId'], $object[8]);
+        $this->assertEquals(App::currentLocale(), $object[9]);
+    }
+
+    public function testDeletedUserChange()
+    {
+        $user = $this->createAdminUser();
+        $token = $user->createToken('test')->plainTextToken;
+
+        $this
+            ->withHeader('Authorization', "Bearer $token")
+            ->delete("/api/v1/users/$user->id_bigint")
+            ->assertStatus(204);
+
+        $change = Change::query()->first();
+        $this->assertEquals($user->id_bigint, $change->user_id_bigint);
+        $this->assertEquals($user->id_bigint, $change->service_body_id_bigint);
+        $this->assertEquals(App::currentLocale(), $change->lang_enum);
+        $this->assertEquals('c_comdef_user', $change->object_class_string);
+        $this->assertEquals($user->id_bigint, $change->before_id_bigint);
+        $this->assertEquals(App::currentLocale(), $change->before_lang_enum);
+        $this->assertNull($change->after_id_bigint);
+        $this->assertNull($change->after_lang_enum);
+        $this->assertEquals('comdef_change_type_delete', $change->change_type_enum);
+        $this->assertNotNull($change->before_object);
+        $this->assertNull($change->after_object);
+    }
+}
