141f3845ea36c6d9709b3b7e4f96c5b012bb639d
Patrick Joyce <pjaudiomv@gmail.com>
2022-10-02 13:11:32 -0400

    add swagger
    Defautl false
    
    set to true
    
    force ssl
    
    maybe
    
    force ssl
    
    formats
    
    force ssl
    
    add back formats
    
    Rel p[ath
    
    dev
    
    make dev
    
    rm l5-swagger
    
    additional ignore
    
    use container for php things
    
    fix gitignore
    
    better make target
    
    add open api controller
    
    regenerate
    
    mv import
    
    rm route
    
    rm route in api.php
    
    initital auth token response
    
    rm collection
    
    rename
    
    add servicebody
    
    implement getting single service body id
    
    fix for multiple service bodies
    
    rename openapi

--- src/app/Http/Controllers/Admin/TokenController.php
+++ src/app/Http/Controllers/Admin/TokenController.php
@@ -11,6 +11,67 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
+/**
+ * @OA\Post(
+ * path="/api/v1/auth/token",
+ * summary="Retrieve Token",
+ * description="Retrieve token by username, password",
+ * operationId="authToken",
+ * tags={"auth"},
+ * @OA\RequestBody(
+ *    required=true,
+ *    description="Pass user credentials",
+ *    @OA\JsonContent(
+ *       required={"username","password"},
+ *       @OA\Property(property="username", type="string", format="username", example="MyUsername"),
+ *       @OA\Property(property="password", type="string", format="password", example="PassWord12345")
+ *    ),
+ * ),
+ * @OA\Response(
+ *    response=200,
+ *    description="Returns when user is authenticated",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="token", type="string", example="2|tR6PIqa8tiBJWMu4zyb3qw4eECuERjLd7xeLKgBu"),
+ *       @OA\Property(property="expires_at", type="integer", example="1667342171"),
+ *    )
+ * ),
+* @OA\Response(
+ *     response=422,
+ *     description="Validation error",
+ *     @OA\JsonContent(
+ *        @OA\Property(property="message", type="string", example="The username field is required. (and 1 more error)"),
+ *        @OA\Property(
+ *           property="errors",
+ *           type="object",
+ *           @OA\Property(
+ *              property="username",
+ *              type="array",
+ *              @OA\Items(
+ *                 type="string",
+ *                 example="The email field is required.",
+ *              )
+ *           ),
+ *           @OA\Property(
+ *              property="password",
+ *              type="array",
+ *              @OA\Items(
+ *                 type="string",
+ *                 example="The password field is required.",
+ *              )
+ *           )
+ *        )
+ *     )
+ *     ),
+ * @OA\Response(
+ *    response=401,
+ *    description="Returns when user is not authenticated",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="The provided credentials are incorrect."),
+ *    )
+ * )
+ * )
+ */
+
 class TokenController extends Controller
 {
     private UserRepositoryInterface $userRepository;
--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -7,6 +7,27 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="BMLT - OpenAPI 3.0",
+ *    description="BMLT Admin API Documentation",
+ *    version="1.0.0",
+ *    @OA\License(
+ *        name="MIT",
+ *        url="https://github.com/bmlt-enabled/bmlt-root-server/blob/main/LICENSE"
+ *    )
+ * )
+ * @OA\Server(
+ *      url="https://latest.aws.bmlt.app/main_server",
+ *      description="BMLT Admin API Server"
+ * )
+ * @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      type="http",
+ *      scheme="bearer"
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
