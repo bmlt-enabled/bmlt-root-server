99dfd7817932bc8e1d6d0d7828e9ed73a0a4cd26
jbraswell <10187286+jbraswell@users.noreply.github.com>
2022-10-06 10:15:46 -0400

    set swagger ui servers with plugin (#700)

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,6 +1,17 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
+  const ServerMutatorPlugin = (system) => ({
+    rootInjects: {
+      setServers: (servers) => {
+        const jsonSpec = system.getState().toJSON().spec.json;
+        const newJsonSpec = Object.assign({}, jsonSpec, { servers });
+
+        return system.specActions.updateJsonSpec(newJsonSpec);
+      }
+    }
+  });
+
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "https://raw.githubusercontent.com/bmlt-enabled/bmlt-root-server/main/src/storage/api-docs/api-docs.json",
@@ -11,9 +22,18 @@ window.onload = function() {
       SwaggerUIStandalonePreset
     ],
     plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
+      SwaggerUIBundle.plugins.DownloadUrl,
+      ServerMutatorPlugin
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    onComplete: () => {
+      window.ui.setServers([
+        {url: 'https://latest.aws.bmlt.app/main_server/', description: 'Latest'},
+        {url: 'https://unstable.aws.bmlt.app/main_server/', description: 'Unstable'},
+        {url: 'https://gyro.aws.bmlt.app/main_server/', description: 'Gyro'},
+        {url: 'https://{domain}', description: 'Custom', variables: { domain: { default: '' } } },
+      ]);
+    }
   });
 
   //</editor-fold>
