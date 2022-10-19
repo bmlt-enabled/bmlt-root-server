e022e4033d8f218d865517a45277a86341d9ba92
jbraswell <10187286+jbraswell@users.noreply.github.com>
2022-10-19 13:46:59 -0400

    use relative path path for api base url (#750)

--- src/resources/views/frontend.blade.php
+++ src/resources/views/frontend.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BMLT Root Server</title>
     <script>
-        var baseUrl = '{{ url('/') }}';
+        var baseUrl = '{{ request()->getBaseUrl() }}';
     </script>
 
     @viteReactRefresh
