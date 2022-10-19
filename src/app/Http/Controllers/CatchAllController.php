74ccac248ee7c33ec401137a246aee6a11a6d3d9
jbraswell <10187286+jbraswell@users.noreply.github.com>
2022-10-19 14:19:18 -0400

    allow access to semantic workshop when new ui is enabled (#751)

--- src/app/Http/Controllers/CatchAllController.php
+++ src/app/Http/Controllers/CatchAllController.php
@@ -8,6 +8,11 @@ use App\Http\Controllers\Legacy\LegacyPathInfo;
 
 class CatchAllController extends Controller
 {
+    private static array $allowedLegacyPathEndings = [
+        '/semantic/index.php',
+        '/client_interface/html/index.php',
+    ];
+
     public function all(Request $request): Response
     {
         return self::handle($request);
@@ -15,18 +20,38 @@ class CatchAllController extends Controller
 
     public static function handle(Request $request): Response
     {
+        $pathInfo = LegacyPathInfo::parse($request);
+
         if (legacy_config('new_ui_enabled')) {
+            if (self::isAllowedLegacyPath($pathInfo) && file_exists($pathInfo->path)) {
+                return self::legacyResponse($pathInfo);
+            }
+
             return response()->view('frontend');
         }
 
-        $pathInfo = LegacyPathInfo::parse($request);
-
         if (file_exists($pathInfo->path)) {
-            return response()
-                ->view('legacy', ['includePath' => $pathInfo->path])
-                ->header('Content-Type', $pathInfo->contentType);
+            return self::legacyResponse($pathInfo);
         }
 
         abort(404);
+    }
+
+    private static function isAllowedLegacyPath(LegacyPathInfo $pathInfo): bool
+    {
+        foreach (self::$allowedLegacyPathEndings as $allowedPathEnding) {
+            if (str_ends_with($pathInfo->path, $allowedPathEnding)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static function legacyResponse(LegacyPathInfo $pathInfo): Response
+    {
+        return response()
+            ->view('legacy', ['includePath' => $pathInfo->path])
+            ->header('Content-Type', $pathInfo->contentType);
     }
 }
