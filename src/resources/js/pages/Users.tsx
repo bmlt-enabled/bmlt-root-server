69260a98523f4863a75f3388a51b8d2ec30c0793
Jimmy Neville <jimmyeneville@gmail.com>
2022-11-08 07:58:42 -0500

    Destructure imports

--- src/resources/js/pages/Users.tsx
+++ src/resources/js/pages/Users.tsx
@@ -1,7 +1,4 @@
-import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
+import { Box, FormControl, InputLabel, MenuItem } from '@mui/material';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { User } from 'bmlt-root-server-client';
 import { useEffect, useState } from 'react';
