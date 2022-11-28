1d1273e9382dec96369ed3b7694fa6ee0edf58cb
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-11-28 12:19:10 -0500

    wip

--- src/tests/Feature/Aggregator/ImportServiceBodyTest.php
+++ src/tests/Feature/Aggregator/ImportServiceBodyTest.php
@@ -23,7 +23,7 @@ class ImportServiceBodyTest extends TestCase
     {
         return new ExternalServiceBody([
             'id' => '171',
-            'parent_id' => '20',
+            'parent_id' => '0',
             'name' => 'Trans Umbrella Area',
             'description' => 'description',
             'type' => 'AS',
@@ -39,7 +39,6 @@ class ImportServiceBodyTest extends TestCase
         return $repository->create([
             'root_server_id' => $rootServerId,
             'source_id' => $sourceId,
-            'sb_owner' => 21,
             'name_string' => 'some name',
             'description_string' => 'some description',
             'sb_type' => 'some type',
@@ -76,10 +75,10 @@ class ImportServiceBodyTest extends TestCase
 
         $external = $this->external();
 
-        $repository = new ServiceBodyRepository();
         $this->create($rootServer1->id, $external->id);
         $this->create($rootServer2->id, $external->id);
 
+        $repository = new ServiceBodyRepository();
         $repository->import($rootServer1->id, collect([$external]));
 
         $all = $repository->search();
@@ -105,12 +104,12 @@ class ImportServiceBodyTest extends TestCase
 
         $external = $this->external();
 
-        $repository = new ServiceBodyRepository();
         $this->create($rootServer1->id, $external->id);
         $this->create($rootServer1->id, $external->id + 1);
         $this->create($rootServer2->id, $external->id);
         $this->create($rootServer3->id, $external->id);
 
+        $repository = new ServiceBodyRepository();
         $repository->import($rootServer1->id, collect([$external]));
 
         $all = $repository->search();
@@ -135,5 +134,47 @@ class ImportServiceBodyTest extends TestCase
         $this->assertFalse($external->isEqual($db));
     }
 
-    // TODO test sb_owner assignments
+    public function testSbOwnerAssignment()
+    {
+        LegacyConfig::set('aggregator_mode_enabled', true);
+        $rootServer1 = $this->createRootServer(1);
+        $rootServer2 = $this->createRootServer(2);
+
+        $externalTop = $this->external();
+        $externalTop->id = 1;
+        $externalTop->parentId = 0;
+
+        $externalMiddle = $this->external();
+        $externalMiddle->id = 2;
+        $externalMiddle->parentId = 1;
+
+        $externalBottom = $this->external();
+        $externalBottom->id = 3;
+        $externalBottom->parentId = 2;
+
+        $this->create($rootServer2->id, $externalTop->id);
+        $this->create($rootServer2->id, $externalMiddle->id);
+        $this->create($rootServer2->id, $externalBottom->id);
+
+        $repository = new ServiceBodyRepository();
+        $repository->import($rootServer1->id, collect([$externalTop, $externalMiddle, $externalBottom]));
+
+        $all = $repository->search(rootServersInclude: [$rootServer1->id]);
+        $this->assertEquals(3, $all->count());
+        $serviceBodyTop = $all->firstWhere('source_id', $externalTop->id);
+        $serviceBodyMiddle = $all->firstWhere('source_id', $externalMiddle->id);
+        $serviceBodyBottom = $all->firstWhere('source_id', $externalBottom->id);
+        $this->assertEquals(0, $serviceBodyTop->sb_owner);
+        $this->assertEquals($serviceBodyTop->id_bigint, $serviceBodyMiddle->sb_owner);
+        $this->assertEquals($serviceBodyMiddle->id_bigint, $serviceBodyBottom->sb_owner);
+
+        $all = $repository->search(rootServersInclude: [$rootServer2->id]);
+        $this->assertEquals(3, $all->count());
+        $serviceBodyTop = $all->firstWhere('source_id', $externalTop->id);
+        $serviceBodyMiddle = $all->firstWhere('source_id', $externalMiddle->id);
+        $serviceBodyBottom = $all->firstWhere('source_id', $externalBottom->id);
+        $this->assertEquals(0, $serviceBodyTop->sb_owner);
+        $this->assertEquals(0, $serviceBodyMiddle->sb_owner);
+        $this->assertEquals(0, $serviceBodyBottom->sb_owner);
+    }
 }
