6b38a6b269ec36b66980825647a02cf4b1773e82
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2023-04-02 12:01:25 -0400

    ignore all name starting with dot when calculating server langs (#888)

--- src/app/Http/Controllers/Query/SwitcherController.php
+++ src/app/Http/Controllers/Query/SwitcherController.php
@@ -522,7 +522,7 @@ class SwitcherController extends Controller
         return new JsonResponse([[
             'version' => config('app.version'),
             'versionInt' => strval((intval($versionArray[0]) * 1000000) + (intval($versionArray[1]) * 1000) + intval(strstr($versionArray[2], '-', true) ?: $versionArray[2])),
-            'langs' => collect(scandir(base_path('lang')))->reject(fn ($dir) => $dir == '.' || $dir == '..')->sort()->join(','),
+            'langs' => collect(scandir(base_path('lang')))->reject(fn ($dir) => str_starts_with($dir, '.'))->sort()->join(','),
             'nativeLang' => config('app.locale'),
             'centerLongitude' => strval(legacy_config('search_spec_map_center_longitude')),
             'centerLatitude' => strval(legacy_config('search_spec_map_center_latitude')),
