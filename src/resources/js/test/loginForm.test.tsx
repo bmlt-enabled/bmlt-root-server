c5c5eea8e739a26c414d5be91bfec8fa2aac7188
GarrettWeinberg <weinberg135@gmail.com>
2023-04-30 17:19:38 -0400

    putting broken test back to not break stuff in other branches

---
+++ src/resources/js/test/loginForm.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+
+import { strings } from '../localization';
+import LoginForm from '../partials/forms/LoginForm';
+import { provideTheme } from './utils/provideTheme';
+
+describe('Login', () => {
+  beforeAll(() => {
+    render(
+      provideTheme(
+        <LoginForm
+          handleOnSubmit={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+        />,
+      ),
+    );
+  });
+  it('renders form title', () => {
+    expect(
+      screen.getByRole('heading', {
+        level: 3,
+      }),
+    ).toHaveTextContent(strings.loginTitle!);
+  });
+});
