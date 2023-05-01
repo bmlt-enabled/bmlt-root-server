6d636f1dfe4340b0c09c6b849f8aed290994e9cb
GarrettWeinberg <weinberg135@gmail.com>
2023-05-01 06:50:53 -0400

    prettier and lint

--- src/resources/js/test/utils/provideTheme.tsx
+++ src/resources/js/test/utils/provideTheme.tsx
@@ -1,8 +1,11 @@
-import { ThemeProvider } from '@mui/material/styles';
-import { ReactElement } from 'react';
+// import { ThemeProvider } from '@mui/material/styles';
+// import { ReactElement } from 'react';
 
-import { theme } from '../../theme';
+// import { theme } from '../../theme';
 
-export const provideTheme = (ui: ReactElement): ReactElement => {
-  return <ThemeProvider theme={theme}>{ui}</ThemeProvider>;
-};
+// export const provideTheme = (ui: ReactElement): ReactElement => {
+//   return <ThemeProvider theme={theme}>{ui}</ThemeProvider>;
+// };
+
+// adding this line to make lint happy. remove it when you add tests
+export {};
