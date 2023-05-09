584e970e4cbcdd4e29c5e0407074ec56391c93c4
GarrettWeinberg <weinberg135@gmail.com>
2023-05-08 22:21:11 -0400

    attempt to mock functions and run additional tests

--- src/resources/js/test/login.test.tsx
+++ src/resources/js/test/login.test.tsx
@@ -1,9 +1,39 @@
-import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 
 import { Login } from '../pages/Login';
 import { provideTheme } from './utils/provideTheme';
+
+// mock login function from RootServerApi
+vi.mock('../RootServerApi', () => {
+  const login = vi.fn();
+  login.mockResolvedValue({
+    token: 'token',
+  });
+  return {
+    login,
+  };
+});
+
+// mock handleErrors function from RootServerApi
+vi.mock('../RootServerApi', () => {
+  const handleErrors = vi.fn();
+  handleErrors.mockResolvedValue({
+    handleAuthenticationError: (error: any) => {
+      error.message = 'Invalid username or password';
+    },
+    handleValidationError: (error: any) => {
+      error.errors = {
+        username: ['Username is required'],
+        password: ['Password is required'],
+      };
+    },
+  });
+  return {
+    handleErrors,
+  };
+});
 
 describe('Login', () => {
   beforeEach(() => {
@@ -15,7 +45,10 @@ describe('Login', () => {
       ),
     );
   });
-  afterEach(cleanup);
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
   it('onSubmit check validation for empty username and password fields', async () => {
     fireEvent.submit(
       screen.getByRole('button', {
@@ -26,5 +59,43 @@ describe('Login', () => {
     expect(await screen.findByText(/password is required/i)).toBeInTheDocument();
   });
 
-  // need to test react-hook-form and test for validation
+  it('onSubmit check validation for empty username field', async () => {
+    fireEvent.input(screen.getByLabelText(/username/i), {
+      target: { value: '' },
+    });
+    fireEvent.submit(
+      screen.getByRole('button', {
+        name: /log in/i,
+      }),
+    );
+    expect(await screen.findByText(/username is required/i)).toBeInTheDocument();
+  });
+
+  it('onSubmit check validation for empty password field', async () => {
+    fireEvent.input(screen.getByLabelText(/password/i), {
+      target: { value: '' },
+    });
+    fireEvent.submit(
+      screen.getByRole('button', {
+        name: /log in/i,
+      }),
+    );
+    expect(await screen.findByText(/password is required/i)).toBeInTheDocument();
+  });
+
+  it('onSubmit check for error message when username or password is incorrect', async () => {
+    fireEvent.input(screen.getByLabelText(/username/i), {
+      target: { value: 'username' },
+    });
+    fireEvent.input(screen.getByLabelText(/password/i), {
+      target: { value: 'password' },
+    });
+    fireEvent.submit(
+      screen.getByRole('button', {
+        name: /log in/i,
+      }),
+    );
+    expect(await screen.findByText(/username is required/i)).toBeInTheDocument();
+    expect(await screen.findByText(/password is required/i)).toBeInTheDocument();
+  });
 });
