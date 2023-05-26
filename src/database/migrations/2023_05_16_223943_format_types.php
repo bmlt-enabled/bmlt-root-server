c88e0d8dc1aa41b8ac8ec4627cb021d0592bfc33
otrok7 <50595291+otrok7@users.noreply.github.com>
2023-05-26 07:03:56 +0200

    Update 2023_05_16_223943_format_types.php

--- src/database/migrations/2023_05_16_223943_format_types.php
+++ src/database/migrations/2023_05_16_223943_format_types.php
@@ -19,24 +19,21 @@ return new class extends Migration
      */
     public function up()
     {
-        if (Schema::hasTable('comdef_format_types')) {
-        } else {
-            Schema::create('comdef_format_types', function (Blueprint $table) {
-                $table->string('key_string', 10);
-                $table->string('api_key', 256);
-                $table->index('key_string');
-                $table->index('api_key');
-            });
-            if (!legacy_config('aggregator_mode_enabled')) {
-                // aggregator mode does not need any stock data
-                DB::table('comdef_format_types')->insert([
-                    ['key_string' => 'FC1', 'api_key' => 'MEETING_FORMAT'],
-                    ['key_string' => 'FC2', 'api_key' =>  'LOCATION'],
-                    ['key_string' => 'FC3', 'api_key' => 'COMMON_NEEDS_OR_RESTRICTION'],
-                    ['key_string' => 'O', 'api_key' => 'OPEN_OR_CLOSED'],
-                    ['key_string' => 'LANG', 'api_key' => 'LANGUAGE'],
-                ]);
-            }
+        Schema::create('comdef_format_types', function (Blueprint $table) {
+            $table->string('key_string', 10);
+            $table->string('api_key', 256);
+            $table->index('key_string');
+            $table->index('api_key');
+        });
+        if (!legacy_config('aggregator_mode_enabled')) {
+            // aggregator mode does not need any stock data
+            DB::table('comdef_format_types')->insert([
+                ['key_string' => 'FC1', 'api_key' => 'MEETING_FORMAT'],
+                ['key_string' => 'FC2', 'api_key' =>  'LOCATION'],
+                ['key_string' => 'FC3', 'api_key' => 'COMMON_NEEDS_OR_RESTRICTION'],
+                ['key_string' => 'O', 'api_key' => 'OPEN_OR_CLOSED'],
+                ['key_string' => 'LANG', 'api_key' => 'LANGUAGE'],
+            ]);
         }
     }
 
@@ -47,6 +44,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('comdef_format_types');
     }
 };
