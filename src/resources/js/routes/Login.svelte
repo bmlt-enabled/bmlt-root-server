3fec2e5d0eee04faec011dc99f8cdcd763be20a8
Patrick Joyce <pjaudiomv@gmail.com>
2024-06-14 13:13:31 -0400

    login
    rm map

--- src/resources/js/routes/Login.svelte
+++ src/resources/js/routes/Login.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import { theme } from '../stores/themeStore';
+  import LoginForm from '../components/LoginForm.svelte';
 </script>
 
 <section class={$theme}>
   <div id="export-form">
     <h1>Lets Login</h1>
     <div id="footer">
-      <a href="#/" class="footer-link">Home</a>
+      <LoginForm />
     </div>
   </div>
 </section>
