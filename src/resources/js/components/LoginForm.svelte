f3f225accd0084ce7040f239b498af763a646d6a
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2024-07-03 14:04:17 -0400

    refactor forms using felte with yum validation

--- src/resources/js/components/LoginForm.svelte
+++ src/resources/js/components/LoginForm.svelte
@@ -1,110 +1,103 @@
 <script lang="ts">
-  import { DarkMode } from 'flowbite-svelte';
-  import { writable } from 'svelte/store';
+  import { validator } from '@felte/validator-yup';
+  import { createForm } from 'felte';
+  import { Button, DarkMode, Helper, Input, Label, P, Select } from 'flowbite-svelte';
   import { push } from 'svelte-spa-router';
+  import * as yup from 'yup';
 
   import RootServerApi from '../lib/RootServerApi';
   import { translations } from '../stores/localization';
   import type { ApiCredentialsStore } from '../stores/apiCredentials';
 
   export let apiCredentials: ApiCredentialsStore;
+
   const globalSettings = settings;
-  const languageOptions = writable(Object.entries(globalSettings.languageMapping));
-  let errorMessage = '';
-  let username = '';
-  let password = '';
+  const languageOptions = Object.entries(globalSettings.languageMapping).map((lang) => ({ value: lang[0], name: lang[1] }));
   let selectedLanguage = translations.getLanguage();
-  const validationMessage = {
-    username: '',
-    password: ''
-  };
+  let errorMessage: string | undefined;
+
+  const { form, data, errors } = createForm({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    onSubmit: async (values) => {
+      await apiCredentials.login(values.username, values.password);
+    },
+    onSuccess: () => {
+      push('/');
+    },
+    onError: async (error) => {
+      await RootServerApi.handleErrors(error as Error, {
+        handleAuthenticationError: () => {
+          errorMessage = $translations.invalidUsernameOrPassword;
+        },
+        handleValidationError: (error) => {
+          errors.set({
+            username: (error?.errors?.username ?? []).join(' '),
+            password: (error?.errors?.password ?? []).join(' ')
+          });
+        }
+      });
+    },
+    extend: validator({
+      schema: yup.object({
+        username: yup.string().required(),
+        password: yup.string().required()
+      })
+    })
+  });
 
   $: if (selectedLanguage) {
     translations.setLanguage(selectedLanguage);
   }
 
-  async function handleLogin() {
-    try {
-      await apiCredentials.login(username, password);
-      push('/');
-    } catch (error: any) {
-      errorMessage = '';
-      validationMessage.username = '';
-      validationMessage.password = '';
-      await RootServerApi.handleErrors(error, {
-        handleAuthenticationError: (error) => {
-          errorMessage = error.message;
-        },
-        handleValidationError: (error) => {
-          validationMessage.username = (error?.errors?.username ?? []).join(' ');
-          validationMessage.password = (error?.errors?.password ?? []).join(' ');
-        }
-      });
-    }
+  $: if ($data) {
+    errorMessage = '';
   }
 </script>
 
-<section class="bg-gray-50 dark:bg-gray-900">
-  <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-    <div class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
-      BMLT Root Server ({globalSettings.version})
-    </div>
-    <div class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
-      <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">{$translations.adminTitle} {$translations.loginTitle}</h1>
-        <form on:submit|preventDefault={handleLogin} class="space-y-4 md:space-y-6">
-          <div>
-            <label for="username" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">{$translations.usernameTitle}</label>
-            <input
-              type="text"
-              bind:value={username}
-              id="username"
-              class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-              placeholder=""
-            />
-          </div>
-          <div class="font-medium text-primary-600 dark:text-primary-500">
-            {validationMessage.username}
-          </div>
-          <div>
-            <label for="password" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">{$translations.passwordTitle}</label>
-            <input
-              type="password"
-              bind:value={password}
-              id="password"
-              placeholder="••••••••"
-              class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            />
-          </div>
-          <div class="font-medium text-primary-600 dark:text-primary-500">
-            {validationMessage.password}
-          </div>
-          {#if globalSettings.isLanguageSelectorEnabled}
-            <div>
-              <label for="language" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Select a language</label>
-              <select
-                id="language"
-                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                bind:value={selectedLanguage}
-              >
-                <option value="" disabled>Choose a language</option>
-                {#each $languageOptions as [code, name]}
-                  <option value={code}>{name}</option>
-                {/each}
-              </select>
-            </div>
-          {/if}
-          <button
-            type="submit"
-            class="w-full rounded-lg bg-primary-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >Sign in</button
-          >
-          <p class="font-medium text-primary-600 dark:text-primary-500">
-            {errorMessage}
-          </p>
-        </form>
-      </div>
-    </div>
-    <DarkMode class="ml-4" />
+<div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+  <div class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
+    {$translations.rootServerTitle} ({globalSettings.version})
   </div>
-</section>
+  <div class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
+    <div class="m-8">
+      <form use:form>
+        <div class="mb-4">
+          <Label for="username" class="mb-2">{$translations.usernameTitle}</Label>
+          <Input type="text" name="username" />
+          <Helper class="mt-2" color="red">
+            {#if $errors.username}
+              {$errors.username}
+            {/if}
+          </Helper>
+        </div>
+        <div class="mb-4">
+          <Label for="password" class="mb-2">{$translations.passwordTitle}</Label>
+          <Input type="password" name="password" />
+          <Helper class="mt-2" color="red">
+            {#if $errors.password}
+              {$errors.password}
+            {/if}
+          </Helper>
+        </div>
+        {#if globalSettings.isLanguageSelectorEnabled}
+          <div class="mb-4">
+            <Label for="languageSelection" class="mb-2">{$translations.languageSelectTitle}</Label>
+            <Select items={languageOptions} bind:value={selectedLanguage} />
+          </div>
+        {/if}
+        {#if errorMessage}
+          <div class="mb-4">
+            <P color="text-red-700 dark:text-red-500">{errorMessage}</P>
+          </div>
+        {/if}
+        <div class="mb-2">
+          <Button type="submit">{$translations.loginVerb}</Button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <DarkMode class="ml-4" />
+</div>
