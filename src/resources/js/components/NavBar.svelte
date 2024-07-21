01305ed87f3bb60965f65ccb026354d220bb9370
Patrick Joyce <pjaudiomv@gmail.com>
2024-07-21 13:12:50 -0400

    revert navbar

--- src/resources/js/components/NavBar.svelte
+++ src/resources/js/components/NavBar.svelte
@@ -24,16 +24,7 @@
   <NavUl>
     <NavLi href="#/">{$translations.homeTitle}</NavLi>
     <NavLi href="#/meetings">{$translations.meetingsTitle}</NavLi>
-    <!--  // TODO:  this currently hides nav item but user can still access route-->
-    <!--  // TODO:  although it will say no users. Seems like we could update route guard to account for this -->
-    <!--    {#if $usersData.length > 1 || $authenticatedUser?.type === 'admin'}-->
-    <!--      <NavLi href="#/users">{$translations.usersTitle}</NavLi>-->
-    <!--    {/if}-->
-    <!--    {#if $serviceBodiesData.length > 0 || $authenticatedUser?.type === 'admin'}-->
-    <!--      <NavLi href="#/service-bodies">{$translations.serviceBodiesTitle}</NavLi>-->
-    <!--    {/if}-->
     <NavLi href="#/users">{$translations.usersTitle}</NavLi>
-    <NavLi href="#/service-bodies">{$translations.serviceBodiesTitle}</NavLi>
     <NavLi href="#" on:click={logout}>{$translations.logout}</NavLi>
   </NavUl>
 </Navbar>
