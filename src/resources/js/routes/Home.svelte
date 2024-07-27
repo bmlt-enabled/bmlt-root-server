682025764b48a6d606effe3f5cf1eb8375146abf
Alan B <918773+alanb2718@users.noreply.github.com>
2024-07-27 16:43:20 -0700

    add more unit tests for language selector, Users tab (#977)

--- src/resources/js/routes/Home.svelte
+++ src/resources/js/routes/Home.svelte
@@ -6,9 +6,11 @@
   // eslint-disable-next-line
   $$restProps;
 
-  import Nav from '../components/NavBar.svelte';
   import { Card } from 'flowbite-svelte';
+
   import { authenticatedUser } from '../stores/apiCredentials';
+  import Nav from '../components/NavBar.svelte';
+  import { translations } from '../stores/localization';
 </script>
 
 <Nav />
@@ -16,7 +18,8 @@
 {#if $authenticatedUser}
   <Card class="mx-auto my-8 w-full max-w-lg bg-white p-8 text-center shadow-lg dark:bg-gray-800">
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      Welcome {$authenticatedUser.displayName}
+      {$translations.welcome}
+      {$authenticatedUser.displayName}
     </h5>
   </Card>
 {/if}
--- src/resources/js/tests/Languages.spec.ts
+++ src/resources/js/tests/Languages.spec.ts
@@ -1,9 +1,11 @@
 import { beforeAll, describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 import App from '../App.svelte';
 import { setupMocks, sharedAfterEach } from './sharedDataAndMocks';
+import { translations } from '../stores/localization';
 
 beforeAll(async () => {
   setupMocks();
@@ -11,10 +13,13 @@ beforeAll(async () => {
 
 afterEach(async () => {
   await sharedAfterEach();
+  // put the default language back to English in case a test changed it
+  translations.setLanguage('en');
 });
 
 describe('language selection tests', () => {
-  test('check language selection menu', async () => {
+  test('test language selection menu with a successful login', async () => {
+    const user = userEvent.setup();
     render(App);
     const langs: HTMLSelectElement = await screen.findByRole('combobox', { name: 'Select Language' });
     expect(langs.length).toBe(4);
@@ -22,14 +27,48 @@ describe('language selection tests', () => {
     expect(langs.item(1)?.label).toBe('English');
     expect(langs.item(2)?.label).toBe('Deutsch');
     expect(langs.item(3)?.label).toBe('Français');
+    await userEvent.selectOptions(langs, ['Deutsch']);
+    // login screen should now be in German, including the Language Selection menu title
+    expect(await screen.findByRole('combobox', { name: 'Sprache auswählen' })).toBeEnabled();
+    await user.type(await screen.findByRole('textbox', { name: 'Benutzername' }), 'serveradmin');
+    await user.type(await screen.findByLabelText('Passwort'), 'serveradmin-password');
+    expect(await screen.findByRole('textbox', { name: 'Benutzername' })).toHaveDisplayValue('serveradmin');
+    expect(await screen.findByLabelText('Passwort')).toHaveDisplayValue('serveradmin-password');
+    await user.click(await screen.findByRole('button', { name: 'Anmelden' }));
+    // after a successful login, we should see 'Willkommen Server Administrator' and the navbar
+    expect(await screen.findByText('Willkommen Server Administrator')).toBeInTheDocument();
+    // check for one element in the navbar
+    // TODO: shouldn't need to say hidden!
+    expect(await screen.findByRole('link', { name: 'Benutzer', hidden: true })).toBeEnabled();
+    // Log out
+    await user.click(await screen.findByRole('link', { name: 'Abmelden', hidden: true }));
+    // Make sure we're back at the login screen (which should still be in German)
+    expect(await screen.findByRole('button', { name: 'Anmelden' })).toBeEnabled();
+    expect(await screen.findByRole('combobox', { name: 'Sprache auswählen' })).toBeEnabled();
+  });
+
+  test('test language selection menu with a bad login', async () => {
+    // just set the language directly for this test -- already tested the language selection menu above
+    translations.setLanguage('de');
+    const user = userEvent.setup();
+    render(App);
+    await user.type(await screen.findByRole('textbox', { name: 'Benutzername' }), 'serveradmin');
+    await user.type(await screen.findByLabelText('Passwort'), 'schlechtes-passwort');
+    await user.click(await screen.findByRole('button', { name: 'Anmelden' }));
+    await screen.findByText('Ungültiger Benutzername oder Passwort.');
   });
 
   test('test isLanguageSelectorEnabled == false', async () => {
     const settings = (global as any).settings;
     settings['isLanguageSelectorEnabled'] = false;
     render(App);
-    // TODO: this isn't right.  Should do findByRole instead, and check that an exception is raised
-    expect(screen.queryByRole('combobox', { name: 'Select Language' })).toBeNull();
+    // TODO: Can this be simplified?  It seems like a baroque way to check that the language selector isn't present.
+    let errorName = '';
+    try {
+      await screen.findByRole('combobox', { name: 'Select Language' });
+    } catch (err: any) {
+      errorName = err.name;
+    }
+    expect(errorName).toBe('TestingLibraryElementError');
   });
-  // TODO: test clicking on German and checking the labels -- see last login test in the react code
 });
