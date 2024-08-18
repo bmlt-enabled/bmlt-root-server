9e908bb69a139b73ed257aa74751f653a5f7ab72
Patrick Joyce <pjaudiomv@gmail.com>
2024-08-18 11:56:58 -0400

    change mime

--- src/app/Http/Controllers/Admin/LogController.php
+++ src/app/Http/Controllers/Admin/LogController.php
@@ -35,7 +35,7 @@ class LogController extends Controller
             },
             'laravel.log.gz',
             [
-                'Content-Type' => 'text/plain; charset=UTF-8'
+                'Content-Type' => 'application/gzip'
             ]
         );
     }
