6f121ac6e9437ba3bd49971663cc1aa5a6aa8dff
Alan B <918773+alanb2718@users.noreply.github.com>
2024-08-25 20:30:00 -0700

    fix bug with multiple saves in Account form, use UserPartialUpdate instead of UserUpdate (#1020)

--- src/resources/js/tests/Account.spec.ts
+++ src/resources/js/tests/Account.spec.ts
@@ -2,14 +2,14 @@ import { beforeAll, beforeEach, describe, test } from 'vitest';
 import { screen } from '@testing-library/svelte';
 import '@testing-library/jest-dom';
 
-import { login, mockSavedUserUpdate, sharedAfterEach, sharedBeforeAll, sharedBeforeEach } from './sharedDataAndMocks';
+import { login, mockSavedUserPartialUpdate, sharedAfterEach, sharedBeforeAll, sharedBeforeEach } from './sharedDataAndMocks';
 
 beforeAll(sharedBeforeAll);
 beforeEach(sharedBeforeEach);
 afterEach(sharedAfterEach);
 
 describe('check content in Account tab when logged in as various users', () => {
-  test('check layout when logged in as serveradmin', async () => {
+  test('check layout and form when logged in as serveradmin', async () => {
     const user = await login('serveradmin', 'Account');
     expect(await screen.findByRole('heading', { name: 'Account Settings', level: 2 })).toBeInTheDocument();
     const applyChanges = screen.getByRole('button', { name: 'Apply Changes' });
@@ -41,26 +41,51 @@ describe('check content in Account tab when logged in as various users', () => {
     expect(password.value).toBe('');
     await user.type(password, 'new password');
     expect(password.value).toBe('new password');
+    expect(applyChanges).not.toBeDisabled();
     await user.click(applyChanges);
-    expect(mockSavedUserUpdate?.displayName).toBe('Grand Poobah');
-    expect(mockSavedUserUpdate?.email).toBe('poobah@bmlt.app');
-    expect(mockSavedUserUpdate?.description).toBe('Main Server Poobah');
-    expect(mockSavedUserUpdate?.username).toBe('serverpoobah');
-    expect(mockSavedUserUpdate?.password).toBe('new password');
-    // Clicking the expand icon is not causing the list to expand, so the commented-out tests below on the list of
-    // editable services bodies fail.  To work around this problem, the service body list is factored out into a separate
-    // component (AccountServiceBodyList), and tested separately.
-    // TODO: if we can get the simulated click on the expand icon to work, the separate AccountServiceBodyList list component
-    // could be folded back in.  Although the code is not too bad as is.
+    expect(applyChanges).toBeDisabled();
+    expect(mockSavedUserPartialUpdate?.displayName).toBe('Grand Poobah');
+    expect(mockSavedUserPartialUpdate?.email).toBe('poobah@bmlt.app');
+    expect(mockSavedUserPartialUpdate?.description).toBe('Main Server Poobah');
+    expect(mockSavedUserPartialUpdate?.username).toBe('serverpoobah');
+    expect(mockSavedUserPartialUpdate?.password).toBe('new password');
+    // Mock clicking the expand icon is not causing the list to expand, so we can't test the list of editable service
+    // bodies.  To work around this problem, the service body list is factored out into a separate component
+    // (AccountServiceBodyList), and tested separately.
+    // TODO: if we can get the simulated click on the expand icon to work, the separate AccountServiceBodyList
+    // component could be folded back in.  Although the code is not too bad as is.
     // const expand = screen.getByRole('button', { name: /service bodies this user can edit/i });
     // await user.click(expand);
-    // TODO: clicking the expand icon is not causing the list to expand, so the following tests fail:
-    // (may be able to change to getByText once expanding the list is working)
-    // expect(await screen.findByText('Northern Zone')).toBeInTheDocument();
-    // expect(await screen.findByText('Big Region')).toBeInTheDocument();
-    // expect(await screen.findByText('Small Region')).toBeInTheDocument();
-    // expect(await screen.findByText('River City Area')).toBeInTheDocument();
-    // expect(await screen.findByText('Mountain Area')).toBeInTheDocument();
-    // expect(await screen.findByText('Rural Area')).toBeInTheDocument();
+    // Now make a further change.  The applyChanges button should be enabled again after a further change.
+    await user.clear(description);
+    await user.type(description, 'Main Server Imperial Wizard');
+    expect(description.value).toBe('Main Server Imperial Wizard');
+    expect(applyChanges).not.toBeDisabled();
+    await user.click(applyChanges);
+    expect(applyChanges).toBeDisabled();
+    expect(mockSavedUserPartialUpdate?.description).toBe('Main Server Imperial Wizard');
+    // other fields in the form should be left intact (including the password)
+    expect(mockSavedUserPartialUpdate?.username).toBe('serverpoobah');
+    expect(mockSavedUserPartialUpdate?.password).toBe('new password');
+  });
+
+  // the following is an edge case -- make sure svelte knows the form is dirty on a second edit just to password
+  test('test making an edit to description, saving, then making an edit to just password', async () => {
+    // no particular reason to use BigRegion rather than serveradmin -- just tests a different kind of user
+    const user = await login('BigRegion', 'Account');
+    const description = (await screen.findByRole('textbox', { name: 'Description' })) as HTMLInputElement;
+    await user.clear(description);
+    await user.type(description, 'Big Region Poobah');
+    const applyChanges = screen.getByRole('button', { name: 'Apply Changes' });
+    expect(applyChanges).not.toBeDisabled();
+    await user.click(applyChanges);
+    expect(applyChanges).toBeDisabled();
+    expect(mockSavedUserPartialUpdate?.description).toBe('Big Region Poobah');
+    const password = screen.getByLabelText('Password') as HTMLInputElement;
+    await user.type(password, 'poobah password');
+    expect(applyChanges).not.toBeDisabled();
+    await user.click(applyChanges);
+    expect(applyChanges).toBeDisabled();
+    expect(mockSavedUserPartialUpdate?.password).toBe('poobah password');
   });
 });
