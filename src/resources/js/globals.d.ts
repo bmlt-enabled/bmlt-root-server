4bc7a79a626b671a5fbd32811503fb59ff0b4fcd
Patrick Joyce <pjaudiomv@gmail.com>
2024-09-09 14:22:35 -0400

    add settings to global constant

--- src/resources/js/globals.d.ts
+++ src/resources/js/globals.d.ts
@@ -1,9 +1,22 @@
 declare const settings: {
   apiBaseUrl: string;
+  autoGeocodingEnabled: true;
+  centerLongitude: number;
+  centerLatitude: number;
+  centerZoom: number;
+  countyAutoGeocodingEnabled: boolean;
+  defaultClosedStatus: boolean;
+  defaultDuration: string;
   defaultLanguage: string;
+  distanceUnits: string;
+  googleApikey: string;
   isLanguageSelectorEnabled: boolean;
   languageMapping: Record<string, string>;
+  meetingStatesAndProvinces: string[];
+  meetingCountiesAndSubProvinces: string[];
+  regionBias: string;
   version: string;
+  zipAutoGeocodingEnabled: false;
 };
 
 declare module 'localized-strings' {
