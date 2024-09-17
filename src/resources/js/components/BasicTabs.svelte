ceff7e6b2d5831ca8a61078cab512b0fb89b5640
Patrick Joyce <pjaudiomv@gmail.com>
2024-09-16 21:54:56 -0400

    add meeting tabs component

---
+++ src/resources/js/components/BasicTabs.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { Button } from 'flowbite-svelte';
+
+  export let tabs: string[] = [];
+  let activeTab: number = 0;
+
+  function setActiveTab(index: number) {
+    activeTab = index;
+  }
+</script>
+
+<div>
+  <div class="flex space-x-4 border-b">
+    {#each tabs as tab, index}
+      <Button color={activeTab === index ? 'light' : 'dark'} on:click={() => setActiveTab(index)} class="py-2" aria-selected={activeTab === index} role="tab">
+        {tab}
+      </Button>
+    {/each}
+  </div>
+  <div class="mt-4">
+    <div class={activeTab === 0 ? '' : 'hidden'}>
+      <slot name="tab-content-0" />
+    </div>
+    <div class={activeTab === 1 ? '' : 'hidden'}>
+      <slot name="tab-content-1" />
+    </div>
+    <div class={activeTab === 2 ? '' : 'hidden'}>
+      <slot name="tab-content-2" />
+    </div>
+    <div class={activeTab === 3 ? '' : 'hidden'}>
+      <slot name="tab-content-3" />
+    </div>
+  </div>
+</div>
+
+<style>
+  .hidden {
+    display: none;
+  }
+</style>
