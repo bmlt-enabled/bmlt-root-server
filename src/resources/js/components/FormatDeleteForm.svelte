1a1d634ed2dee3901b1db609d33a5490d0d5bdd9
Alan B <918773+alanb2718@users.noreply.github.com>
2024-09-30 17:33:15 +0200

    add Formats code (#1058)

---
+++ src/resources/js/components/FormatDeleteForm.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { Button, Checkbox, Helper, P } from 'flowbite-svelte';
+  import { createForm } from 'felte';
+  import { createEventDispatcher } from 'svelte';
+  import { validator } from '@felte/validator-yup';
+  import * as yup from 'yup';
+
+  import RootServerApi from '../lib/RootServerApi';
+  import type { Format } from 'bmlt-root-server-client';
+  import { spinner } from '../stores/spinner';
+  import { translations } from '../stores/localization';
+
+  export let deleteFormat: Format;
+  export let formatName: string;
+  let confirmed = false;
+  let errorMessage: string | undefined;
+
+  const dispatch = createEventDispatcher<{ deleted: { formatId: number } }>();
+
+  const { form } = createForm({
+    initialValues: { formatId: deleteFormat?.id, confirmed: false },
+    onSubmit: async () => {
+      spinner.show();
+      await RootServerApi.deleteFormat(deleteFormat.id);
+    },
+    onError: async (error) => {
+      await RootServerApi.handleErrors(error as Error);
+      spinner.hide();
+    },
+    onSuccess: () => {
+      spinner.hide();
+      dispatch('deleted', { formatId: deleteFormat.id });
+    },
+    extend: validator({
+      schema: yup.object({
+        confirmed: yup.boolean().oneOf([true])
+      })
+    })
+  });
+</script>
+
+<form use:form>
+  <div>
+    <P class="mb-5">{$translations.confirmDeleteFormat}</P>
+    <P class="mb-5">{formatName}</P>
+    <div class="mb-5">
+      <Checkbox bind:checked={confirmed} name="confirmed">{$translations.confirmYesImSure}</Checkbox>
+      <Helper class="mt-4" color="red">
+        {#if errorMessage}
+          {errorMessage}
+        {/if}
+      </Helper>
+    </div>
+    <div class="mb-5">
+      <Button type="submit" class="w-full" disabled={!confirmed ? true : false}>{$translations.delete}</Button>
+    </div>
+  </div>
+</form>
--- src/resources/js/routes/Formats.svelte
+++ src/resources/js/routes/Formats.svelte
@@ -1,11 +1,154 @@
 <script lang="ts">
+  import { Button, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, TableSearch } from 'flowbite-svelte';
+  import { TrashBinOutline } from 'flowbite-svelte-icons';
   import Nav from '../components/NavBar.svelte';
-  import { Card } from 'flowbite-svelte';
+  import FormatModal from '../components/FormatModal.svelte';
+  import FormatDeleteModal from '../components/FormatDeleteModal.svelte';
 
+  import { authenticatedUser } from '../stores/apiCredentials';
+  import { spinner } from '../stores/spinner';
   import { translations } from '../stores/localization';
+  import RootServerApi from '../lib/RootServerApi';
+  // svelte-hack' -- import hacked to get onMount to work correctly for unit tests
+  import { onMount } from 'svelte/internal';
+  import type { Format } from 'bmlt-root-server-client';
+  import FormatForm from '../components/FormatForm.svelte';
+
+  let isLoaded = false;
+  let formats: Format[] = [];
+  let filteredFormats: Format[] = [];
+  let showModal = false;
+  let showDeleteModal = false;
+  let searchTerm = '';
+  let selectedFormat: Format | null;
+  let formatName: string;
+  let deleteFormat: Format;
+
+  const language = translations.getLanguage();
+
+  async function getFormats(): Promise<void> {
+    try {
+      spinner.show();
+      formats = await RootServerApi.getFormats();
+      formats.sort((a, b) => getFormatName(a).localeCompare(getFormatName(b)));
+      isLoaded = true;
+    } catch (error: any) {
+      await RootServerApi.handleErrors(error);
+    } finally {
+      spinner.hide();
+    }
+  }
+
+  function handleAdd() {
+    selectedFormat = null;
+    openModal();
+  }
+
+  function handleEdit(format: Format) {
+    selectedFormat = format;
+    openModal();
+  }
+
+  function handleDelete(event: MouseEvent, format: Format) {
+    event.stopPropagation();
+    deleteFormat = format;
+    showDeleteModal = true;
+  }
+
+  function onSaved(event: CustomEvent<{ format: Format }>) {
+    const format = event.detail.format;
+    const i = formats.findIndex((f) => f.id === format.id);
+    if (i === -1) {
+      formats = [...formats, format].sort((a, b) => getFormatName(a).localeCompare(getFormatName(b)));
+    } else {
+      formats[i] = format;
+    }
+    closeModal();
+  }
+
+  function onDeleted(event: CustomEvent<{ formatId: number }>) {
+    formats = formats.filter((f) => f.id !== event.detail.formatId);
+    showDeleteModal = false;
+  }
+
+  function openModal() {
+    showModal = true;
+  }
+
+  function closeModal() {
+    showModal = false;
+  }
+
+  // Get the name of the format in the current language.  If no translation for that language is available, return the name in
+  // English; if no English version either, than pick anything.
+  export function getFormatName(format: Format): string {
+    const n = format.translations.find((t) => t.language === language);
+    if (n) {
+      return n.name;
+    } else {
+      const e = format.translations.find((t) => t.language === 'en');
+      if (e) {
+        return e.name + ' (' + $translations.noTranslationAvailable + ')';
+      } else {
+        return format.translations[0].name + ' (' + $translations.noTranslationAvailable + ')';
+      }
+    }
+  }
+
+  onMount(getFormats);
+
+  $: {
+    if (selectedFormat) {
+      formatName = getFormatName(selectedFormat);
+    }
+    // prettier-ignore
+    filteredFormats = formats
+      .sort((f1, f2) => getFormatName(f1).localeCompare(getFormatName(f2)))
+      .filter((f) => getFormatName(f).toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1);
+  }
+
+  // In the HTML below, we can assume that the authenticatedUser is the admin -- if not, just show a blank page.
+  // Formats won't appear in the nav bar, but somebody could get to this page directly.  (There isn't any private
+  // information on the formats page, and the server wouldn't let them save, so this wouldn't be a big deal however.)
 </script>
 
 <Nav />
-<Card class="mx-auto my-8 w-full max-w-lg bg-white p-8 text-center shadow-lg dark:bg-gray-800">
-  <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{$translations.formatsTitle}</h2>
-</Card>
+
+<div class="mx-auto max-w-3xl p-2">
+  <h2 class="mb-4 text-center text-xl font-semibold dark:text-white">{$translations.formatsTitle}</h2>
+  {#if isLoaded && $authenticatedUser?.type === 'admin'}
+    {#if formats.length > 0}
+      <TableSearch placeholder={$translations.searchByName} hoverable={true} bind:inputValue={searchTerm}>
+        <TableHead>
+          <TableHeadCell colspan="2">
+            <div class="flex">
+              <div class="mt-2.5 grow">{$translations.nameTitle}</div>
+              <div><Button on:click={() => handleAdd()} class="whitespace-nowrap" aria-label={$translations.addFormat}>{$translations.addFormat}</Button></div>
+            </div>
+          </TableHeadCell>
+        </TableHead>
+        <TableBody>
+          {#each filteredFormats as format}
+            <TableBodyRow on:click={() => handleEdit(format)} class="cursor-pointer" aria-label={$translations.editFormat}>
+              <TableBodyCell class="whitespace-normal">{getFormatName(format)}</TableBodyCell>
+              {#if $authenticatedUser?.type === 'admin'}
+                <TableBodyCell class="text-right">
+                  <Button color="none" on:click={(e) => handleDelete(e, format)} class="text-blue-700 dark:text-blue-500" aria-label={$translations.deleteFormat + ' ' + getFormatName(format)}>
+                    <TrashBinOutline title={{ id: 'deleteFormat', title: $translations.deleteFormat }} ariaLabel={$translations.deleteFormat} />
+                  </Button>
+                </TableBodyCell>
+              {/if}
+            </TableBodyRow>
+          {/each}
+        </TableBody>
+      </TableSearch>
+    {:else}
+      <div class="p-2">
+        <FormatForm {selectedFormat} on:saved={onSaved} />
+      </div>
+    {/if}
+  {/if}
+</div>
+
+<FormatModal bind:showModal {selectedFormat} on:saved={onSaved} on:close={closeModal} />
+<FormatDeleteModal bind:showDeleteModal {deleteFormat} {formatName} on:deleted={onDeleted} />
