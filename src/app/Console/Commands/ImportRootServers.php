222e494744d68109204d1a67870f4ca5c39843c3
Patrick Joyce <pjaudiomv@gmail.com>
2025-05-13 12:48:37 -0400

    change root server json ref

--- src/app/Console/Commands/ImportRootServers.php
+++ src/app/Console/Commands/ImportRootServers.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Http;
 
 class ImportRootServers extends Command
 {
-    protected $signature = 'aggregator:ImportRootServers {--list-url=https://raw.githubusercontent.com/bmlt-enabled/tomato/master/rootServerList.json}';
+    protected $signature = 'aggregator:ImportRootServers {--list-url=https://raw.githubusercontent.com/bmlt-enabled/aggregator/main/rootServerList.json}';
 
     protected $description = 'Import root servers';
 
